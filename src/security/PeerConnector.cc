06523d6794164a57f5710e387d5241124e53ec4e
Alex Rousskov <rousskov@measurement-factory.com>
2018-07-05 19:56:34 -0600

    More const, auto
    Also fixed ConnOpener() constructor API that was pretending to modify
    its second argument.

--- src/security/PeerConnector.cc
+++ src/security/PeerConnector.cc
@@ -471,7 +471,7 @@ Security::PeerConnector::noteWantRead()
 #endif
         // read timeout to avoid getting stuck while reading from a silent server
     AsyncCall::Pointer nil;
-    time_t timeout = Comm::MortalReadTimeout(startTime, negotiationTimeout);
+    const auto timeout = Comm::MortalReadTimeout(startTime, negotiationTimeout);
     commSetConnTimeout(serverConnection(), timeout, nil);
 
     Comm::SetSelect(fd, COMM_SELECT_READ, &NegotiateSsl, new Pointer(this), 0);
