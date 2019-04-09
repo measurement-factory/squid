2c37dcf199c66e1cdfd1d645eb83e5bb3c0d159b
Christos Tsantilas <christos@chtsanti.net>
2019-04-09 14:17:04 +0300

    Fix assertion in errorSend function (errorpage.cc file)
    The ErrorState::BuildHttpReply now can return a locked HttpReply object
    (in the case of ERR_RELAY_REMOTE error) so we can not just delete the
    returned HttpReply object. This patch uses a HttpReplyPointer to
    hold the returned object and delete it if required.

--- src/errorpage.cc
+++ src/errorpage.cc
@@ -649,19 +649,16 @@ errorAppendEntry(StoreEntry * entry, ErrorState * err)
 void
 errorSend(const Comm::ConnectionPointer &conn, ErrorState * err)
 {
-    HttpReply *rep;
     debugs(4, 3, HERE << conn << ", err=" << err);
     assert(Comm::IsConnOpen(conn));
 
-    rep = err->BuildHttpReply();
+    HttpReplyPointer rep(err->BuildHttpReply());
 
     MemBuf *mb = rep->pack();
     AsyncCall::Pointer call = commCbCall(78, 5, "errorSendComplete",
                                          CommIoCbPtrFun(&errorSendComplete, err));
     Comm::Write(conn, mb, call);
     delete mb;
-
-    delete rep;
 }
 
 /**
