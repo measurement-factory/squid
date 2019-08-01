e88d53dde8d88141764017b885f616fa7536c7c8
Alex Rousskov <rousskov@measurement-factory.com>
2019-08-01 16:47:58 -0400

    Stop (ab)using Transient entry flags for Collapsing Requirement mgmnt
    The ENTRY_REQUIRES_COLLAPSING flag was used for relaying "hitting this
    entry carries CF risks right now" info to other workers. That flag is
    not necessary to relay that info because remote workers already know
    whether the entry is attached to one of the shared cache stores.
    Moreover, relaying that flag correctly is difficult and would require a
    different implementation:
    
    * The flag is changed in relatively high-level Store code, possibly
      _after_ we stopped writing, when the transients entry is already
      read-only. This bug leads to an isWriter() assertion in
      Transients::clearCollapsingRequirement() called via a quick or aborted
      StoreEntry::startWriting().
    
    * Basics.flags is not an atomic field. It lacks an "[app]" mark -- its
      readers will not get a reliable value due to race conditions when
      Squid will need to update the Transient entry flags in "append" mode.
    
    * The flag is changed assuming that simply sending stuff to disk is
      sufficient for other workers to read it. This will soon become
      inaccurate because the store index will be updated only after the slot
      is written to disk.
    
    Remote workers now use Store attachment info to set the local StoreEntry
    object flag, ignoring the no-longer-used Transients entry flags.
    
    TODO: The ENTRY_REQUIRES_COLLAPSING flag is still used to tell
    transactions which StoreEntry objects are subject to CF risks. Can we
    use the presence of MemObject::reply_ to relay the same info for
    store-unattached entries (and attachment info for others)? If yes, then
    we can remove ENTRY_REQUIRES_COLLAPSING entirely. See development commit
    1728d5c (which led to master d2a6dcb) for more info/analysis.
    
    Cherry-picked SQUID-448-reading-xient-becomes-writer commit 325afae and
    fixed src/Transients.cc conflicts.

--- src/store.cc
+++ src/store.cc
@@ -1817,13 +1817,6 @@ StoreEntry::startWriting()
 
     rep->body.packInto(this);
     flush();
-
-    // The entry headers are written, new clients
-    // should not collapse anymore.
-    if (hittingRequiresCollapsing()) {
-        setCollapsingRequirement(false);
-        Store::Root().transientsClearCollapsingRequirement(*this);
-    }
 }
 
 char const *
@@ -2099,6 +2092,10 @@ StoreEntry::describeTimestamps() const
 void
 StoreEntry::setCollapsingRequirement(const bool required)
 {
+    if (hittingRequiresCollapsing() == required)
+        return; // no change
+
+    debugs(20, 5, (required ? "adding to " : "removing from ") << *this);
     if (required)
         EBIT_SET(flags, ENTRY_REQUIRES_COLLAPSING);
     else
