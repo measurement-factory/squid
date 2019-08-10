measurement-factory/squid

commit ddc7d7e33160391751049cd85eb5f906623ef108

Fixed StoreMap.cc:204: "anchorAt(anchorId).reading()" assertions
... that occurred because the old code could switch a transient entry
from writing to reading while the corresponding store entry was still
being (or could still be) written to the shared memory cache. For
example:

1. We start as a collapsed writer.
2. We get a response and start writing to disk and shared memory caches.
3. Disk swapout fails (for any reason, including out-of-slots errors).
4. storeSwapOutFileClosed() calls transientsCompleteWriting().
5. transientsCompleteWriting() switches the entry into reading mode
   ... but we are still writing to the memory cache!

There was a somewhat related XXX in master transientsCompleteWriting(),
but what that comment did not say is that if we are writing to two
stores in parallel, then the first transientsCompleteWriting() call
(made when one of the two swapouts ends) makes us a reader prematurely.

An incorrect reader status allows a Controller::syncCollapsed()
notification (to, say, a shared memory cache writer) slip past the
transients->isWriter() guard, eventually reaching the reading()
assertion.

Properly detecting the end of all store writing is difficult because the
two mostly independent writing "threads" may start/finish at seemingly
random times, in many code places, and even in different workers. To
simplify this task, Squid now limits cache writing to the worker
transaction that made the StoreEntry object public. That transaction
creates a writing Transients entry. Other transactions start as
Transients readers. The writer switches to reader when neither memory
nor disk caching can start or continue.

Also simplified Transients entry state. Squid relayed swapout errors via
the Transient entries themselves, but that is not necessary (and it
prevented the cache entry from recovering by writing to another store).
Each store entry can take care of its own swapout status/results. The

src/fs/rock/RockSwapDir.cc
@@ -141,7 +141,8 @@ void Rock::SwapDir::disconnect(StoreEntry &e)
         map->abortWriting(e.swap_filen);
         e.detachFromDisk();
         dynamic_cast<IoState&>(*e.mem_obj->swapout.sio).writeableAnchor_ = NULL;
-        Store::Root().stopSharing(e); // broadcasts after the change
+        CollapsedForwarding::Broadcast(e);
+        e.storeWriterDone();
     } else {
         map->closeForReading(e.swap_filen);
         e.detachFromDisk();
@@ -172,9 +173,12 @@ Rock::SwapDir::doReportStat() const
 }
 
 void
-Rock::SwapDir::finalizeSwapoutSuccess(const StoreEntry &)
+Rock::SwapDir::finalizeSwapoutSuccess(const StoreEntry &e)
 {
-    // nothing to do
+    // nothing to do; handleWriteCompletionSuccess() did everything for us
+    assert(!e.mem_obj ||
+           !e.mem_obj->swapout.sio ||
+           !dynamic_cast<IoState&>(*e.mem_obj->swapout.sio).writeableAnchor_);
 }
 
 void
@@ -903,6 +907,7 @@ Rock::SwapDir::handleWriteCompletionSuccess(const WriteRequest &request)
 
             map->switchWritingToReading(sio.swap_filen);
             // sio.e keeps the (now read) lock on the anchor
+            // storeSwapOutFileClosed() sets swap_status and calls storeWriterDone()
         }
         sio.writeableAnchor_ = NULL;
         sio.finishedWriting(DISK_OK);
@@ -930,7 +935,7 @@ Rock::SwapDir::writeError(StoreIOState &sio)
     map->freeEntry(sio.swap_filen); // will mark as unusable, just in case
 
     if (sio.touchingStoreEntry())
-        Store::Root().stopSharing(*sio.e);
+        CollapsedForwarding::Broadcast(*sio.e);
     // else noop: a fresh entry update error does not affect stale entry readers
 
     // All callers must also call IoState callback, to propagate the error.

src/store_swapout.cc
@@ -47,7 +47,6 @@ storeSwapOutStart(StoreEntry * e)
     debugs(20, 5, "storeSwapOutStart: Begin SwapOut '" << e->url() << "' to dirno " <<
            e->swap_dirn << ", fileno " << std::hex << std::setw(8) << std::setfill('0') <<
            std::uppercase << e->swap_filen);
-    e->swapOutDecision(MemObject::SwapOut::swStarted);
     /* If we start swapping out objects with OutOfBand Metadata,
      * then this code needs changing
      */
@@ -81,6 +80,8 @@ storeSwapOutStart(StoreEntry * e)
     e->lock("storeSwapOutStart");
     /* Pick up the file number if it was assigned immediately */
     e->attachToDisk(mem->swapout.sio->swap_dirn, mem->swapout.sio->swap_filen, SWAPOUT_WRITING);
+
+    e->swapOutDecision(MemObject::SwapOut::swStarted); // after SWAPOUT_WRITING
 
     /* write out the swap metadata */
     storeIOWrite(mem->swapout.sio, buf, mem->swap_hdr_sz, 0, xfree_cppwrapper);
@@ -323,9 +324,9 @@ storeSwapOutFileClosed(void *data, int errflag, StoreIOState::Pointer self)
         ++statCounter.swap.outs;
     }
 
-    Store::Root().transientsCompleteWriting(*e);
     debugs(20, 3, "storeSwapOutFileClosed: " << __FILE__ << ":" << __LINE__);
     mem->swapout.sio = NULL;
+    e->storeWriterDone(); // after updating swap_status
     e->unlock("storeSwapOutFileClosed");
 }
 
@@ -348,9 +349,9 @@ StoreEntry::mayStartSwapOut()
         return false;
     }
 
-    // if we are swapping out or swapped out already, do not start over
+    // if there is a usable disk entry already, do not start over
     if (hasDisk() || Store::Root().hasReadableDiskEntry(*this)) {
-        debugs(20, 3, "already did");
+        debugs(20, 3, "already did"); // we or somebody else created that entry
         swapOutDecision(MemObject::SwapOut::swImpossible);
         return false;
     }
@@ -359,12 +360,19 @@ StoreEntry::mayStartSwapOut()
     // called), do not start over
     if (decision == MemObject::SwapOut::swStarted) {
         debugs(20, 3, "already started");
-        swapOutDecision(MemObject::SwapOut::swImpossible);
         return false;
     }
 
     if (Store::Root().markedForDeletionAndAbandoned(*this)) {
         debugs(20, 3, "marked for deletion and abandoned");
+        swapOutDecision(MemObject::SwapOut::swImpossible);
+        return false;
+    }
+
+    // in SMP mode, restrict caching to StoreEntry publisher to avoid
+    // workers releasing each other caching attempts
+    if (!Store::Root().transientsWriter(*this)) {
+        debugs(20, 5, "yield to entry publisher");
         swapOutDecision(MemObject::SwapOut::swImpossible);
         return false;
     }

src/MemStore.cc
@@ -602,6 +602,19 @@ MemStore::shouldCache(StoreEntry &e) const
         return false;
     }
 
+    // Store::Root() in the next check below is FATALly missing during shutdown
+    if (shutting_down) {
+        debugs(20, 5, "yield to shutdown: " << e);
+        return false;
+    }
+
+    // in SMP mode, restrict caching to StoreEntry publisher to avoid
+    // workers releasing each other caching attempts
+    if (Store::Root().transientsReader(e)) {
+        debugs(20, 5, "yield to entry publisher: " << e);
+        return false;
+    }
+
     const int64_t expectedSize = e.mem_obj->expectedReplySize(); // may be < 0
     const int64_t loadedSize = e.mem_obj->endOffset();
     const int64_t ramSize = max(loadedSize, expectedSize);
@@ -881,8 +894,8 @@ MemStore::completeWriting(StoreEntry &e)
     e.mem_obj->memCache.io = MemObject::ioDone;
     map->closeForWriting(index);
 
-    CollapsedForwarding::Broadcast(e); // before we close our transient entry!
-    Store::Root().transientsCompleteWriting(e);
+    CollapsedForwarding::Broadcast(e);
+    e.storeWriterDone();
 }
 
 void
@@ -921,7 +934,8 @@ MemStore::disconnect(StoreEntry &e)
             map->abortWriting(mem_obj.memCache.index);
             mem_obj.memCache.index = -1;
             mem_obj.memCache.io = MemObject::ioDone;
-            Store::Root().stopSharing(e); // broadcasts after the change
+            CollapsedForwarding::Broadcast(e);
+            e.storeWriterDone();
         } else {
             assert(mem_obj.memCache.io == MemObject::ioReading);
             map->closeForReading(mem_obj.memCache.index);

src/Store.h
@@ -80,6 +80,8 @@ public:
     void memOutDecision(const bool willCacheInRam);
     // called when a decision to cache on disk has been made
     void swapOutDecision(const MemObject::SwapOut::Decision &decision);
+    /// called when a store writer ends its work (successfully or not)
+    void storeWriterDone();
 
     void abort();
     bool makePublic(const KeyScope keyScope = ksDefault);
@@ -286,7 +288,7 @@ public:
 protected:
     typedef Store::EntryGuard EntryGuard;
 
-    void transientsAbandonmentCheck();
+    void storeWritingCheckpoint();
     /// does nothing except throwing if disk-associated data members are inconsistent
     void checkDisk() const;
 

src/Transients.cc
@@ -197,6 +197,7 @@ Transients::clearCollapsingRequirement(const StoreEntry &e)
     const auto idx = e.mem_obj->xitTable.index;
     auto &anchor = map->writeableEntry(idx);
     if (EBIT_TEST(anchor.basics.flags, ENTRY_REQUIRES_COLLAPSING)) {
+        // XXX: Non-atomic, [app]-unmarked flags are off limits in append mode.
         EBIT_CLR(anchor.basics.flags, ENTRY_REQUIRES_COLLAPSING);
         CollapsedForwarding::Broadcast(e);
     }
@@ -265,7 +266,7 @@ Transients::status(const StoreEntry &entry, Transients::EntryStatus &entryStatus
     const auto idx = entry.mem_obj->xitTable.index;
     const auto &anchor = isWriter(entry) ?
                          map->writeableEntry(idx) : map->readableEntry(idx);
-    entryStatus.abortedByWriter = anchor.writerHalted;
+    entryStatus.hasWriter = anchor.writing();
     entryStatus.waitingToBeFreed = anchor.waitingToBeFreed;
     entryStatus.collapsed = EBIT_TEST(anchor.basics.flags, ENTRY_REQUIRES_COLLAPSING);
 }
@@ -273,10 +274,12 @@ Transients::status(const StoreEntry &entry, Transients::EntryStatus &entryStatus
 void
 Transients::completeWriting(const StoreEntry &e)
 {
+    debugs(20, 5, e);
     assert(e.hasTransients());
     assert(isWriter(e));
     map->switchWritingToReading(e.mem_obj->xitTable.index);
     e.mem_obj->xitTable.io = Store::ioReading;
+    CollapsedForwarding::Broadcast(e);
 }
 
 int
@@ -322,7 +325,12 @@ Transients::disconnect(StoreEntry &entry)
         auto &xitTable = entry.mem_obj->xitTable;
         assert(map);
         if (isWriter(entry)) {
-            map->abortWriting(xitTable.index);
+            // completeWriting() was not called, so there could be an active
+            // Store writer out there, but we should not abortWriting() here
+            // because another writer may have succeeded, making readers happy.
+            // If none succeeded, the readers will notice the lack of writers.
+            map->closeForWriting(xitTable.index);
+            CollapsedForwarding::Broadcast(entry);
         } else {
             assert(isReader(entry));
             map->closeForReading(xitTable.index);

src/Transients.h
@@ -19,10 +19,11 @@
 
 typedef Ipc::StoreMap TransientsMap;
 
-/// Keeps track of store entries being delivered to clients that arrived before
-/// those entries were [fully] cached. This SMP-shared table is necessary to
-/// * sync an entry-writing worker with entry-reading worker(s); and
-/// * sync an entry-deleting worker with both entry-reading/writing workers.
+/// A Transients entry allows workers to Broadcast() DELETE requests and swapout
+/// progress updates. In a collapsed forwarding context, it also represents a CF
+/// initiating worker promise to either cache the response or inform the waiting
+/// slaves (via false EntryStatus::hasWriter) that caching will not happen. A
+/// Transients entry itself does not carry response- or Store-specific metadata.
 class Transients: public Store::Controlled, public Ipc::StoreMapCleaner
 {
 public:
@@ -30,7 +31,7 @@ public:
     class EntryStatus
     {
     public:
-        bool abortedByWriter = false; ///< whether the entry was aborted
+        bool hasWriter = false; ///< whether some worker is storing the entry
         bool waitingToBeFreed = false; ///< whether the entry was marked for deletion
         bool collapsed = false; ///< whether the entry allows collapsing
     };

src/ipc/StoreMap.cc
@@ -217,7 +217,6 @@ Ipc::StoreMap::abortWriting(const sfileno fileno)
         debugs(54, 5, "closed clean entry " << fileno << " for writing " << path);
     } else {
         s.waitingToBeFreed = true;
-        s.writerHalted = true;
         s.lock.unlockExclusive();
         debugs(54, 5, "closed dirty entry " << fileno << " for writing " << path);
     }
@@ -827,7 +826,6 @@ Ipc::StoreMapAnchor::rewind()
     memset(&key, 0, sizeof(key));
     basics.clear();
     waitingToBeFreed = false;
-    writerHalted = false;
     // but keep the lock
 }
 

src/ipc/StoreMap.h
@@ -79,8 +79,6 @@ public:
 public:
     mutable ReadWriteLock lock; ///< protects slot data below
     std::atomic<uint8_t> waitingToBeFreed; ///< may be accessed w/o a lock
-    /// whether StoreMap::abortWriting() was called for a read-locked entry
-    std::atomic<uint8_t> writerHalted;
 
     // fields marked with [app] can be modified when appending-while-reading
     // fields marked with [update] can be modified when updating-while-reading

commit 4443485b12eace6e3bd54b01bcb65ad8fc944c73

Fixed mayStartSwapOut() broken in ddc7d7e
The transientsWriter() condition should checked only in SMP case, as
was intended in that revision.

This also broke testRock and testUfs unit tests, because
mayStartSwapout() returned unexpected 'false'. The above fix was not
enough to repair testRock unit case, because Controller::SmpAware()
returns true even for non-smp configurations (one Squid process).
I am not sure whether no-daemon(1 worker)+rock configurations are
legitimate, but I think we need to honour UsingSmp() when deciding
whether rock cache is smp-aware.  This condition is already checked
before creating both shared memory and transients structures.

Alternatively, we could add 'Enabled()' to disk caches, similar
to MemStore::Enabled() and Transients::Enabled(), which would
return false in non-smp case.

src/fs/rock/RockSwapDir.cc
@@ -349,6 +349,12 @@ Rock::SwapDir::parse(int anIndex, char *aPath)
     validateOptions();
 }
 
+bool
+Rock::SwapDir::smpAware() const
+{
+    return UsingSmp();
+}
+
 void
 Rock::SwapDir::reconfigure()
 {

src/fs/rock/RockSwapDir.h
@@ -49,7 +49,7 @@ public:
     virtual void finalizeSwapoutFailure(StoreEntry &);
     virtual void create();
     virtual void parse(int index, char *path);
-    virtual bool smpAware() const { return true; }
+    virtual bool smpAware() const;
     virtual bool hasReadableEntry(const StoreEntry &) const;
 
     // temporary path to the shared memory map of first slots of cached entries

src/store_swapout.cc
@@ -374,7 +374,7 @@ StoreEntry::mayStartSwapOut()
 
     // in SMP mode, restrict caching to StoreEntry publisher to avoid
     // workers releasing each other caching attempts
-    if (!Store::Root().transientsWriter(*this)) {
+    if (Store::Controller::SmpAware() && !Store::Root().transientsWriter(*this)) {
         debugs(20, 5, "yield to entry publisher");
         swapOutDecision(MemObject::SwapOut::swImpossible);
         return false;