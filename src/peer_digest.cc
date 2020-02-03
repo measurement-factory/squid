85a547a7c0314f152ab3d3ef82852ce2fe6fdad7
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2020-02-03 18:28:31 +0300

    eventAdd() contexts analysis: a missing case

--- src/peer_digest.cc
+++ src/peer_digest.cc
@@ -231,7 +231,7 @@ peerDigestNewDelay(const StoreEntry * e)
 static void
 peerDigestSetCheck(PeerDigest * pd, time_t delay)
 {
-    eventAdd("peerDigestCheck", peerDigestCheck, pd, (double) delay, 1);
+    eventAddGlobal2("peerDigestCheck", peerDigestCheck, pd, (double) delay, 1);
     pd->times.next_check = squid_curtime + delay;
     debugs(72, 3, "peerDigestSetCheck: will check peer " << pd->host << " in " << delay << " secs");
 }
