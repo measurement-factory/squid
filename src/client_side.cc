55830f2bd2efb1eb3dc4c1f17e1dfc2faa1d51fa
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2020-06-07 23:54:13 +0300

    Undone 3175e8f
    After ec942e4 the PROXY protocol headers are parsed before creating
    fake CONNECT requests.

--- src/client_side.cc
+++ src/client_side.cc
@@ -1860,17 +1860,6 @@ ConnStateData::parseProxyProtocolHeader()
         if (proxyProtocolHeader_->hasForwardedAddresses()) {
             clientConnection->local = proxyProtocolHeader_->destinationAddress;
             clientConnection->remote = proxyProtocolHeader_->sourceAddress;
-
-            Http::StreamPointer context = pipeline.front();
-            if (context && context->http) {
-                // update the early (possibly fake) request, if any
-                if (auto request = context->http->request) {
-                    // TODO: place this common code into a method
-                    static char ip[MAX_IPSTRLEN];
-                    request->url.host(clientConnection->local.toStr(ip, sizeof(ip)));
-                    request->url.port(clientConnection->local.port());
-                }
-            }
             if ((clientConnection->flags & COMM_TRANSPARENT))
                 clientConnection->flags ^= COMM_TRANSPARENT; // prevent TPROXY spoofing of this new IP.
             debugs(33, 5, "PROXY/" << proxyProtocolHeader_->version() << " upgrade: " << clientConnection);
