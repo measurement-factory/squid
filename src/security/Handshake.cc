b08b11699b84cc4969300056659976252d25bf17
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2020-06-10 14:47:56 +0300

    Do not throw in HandshakeParser::parseHello() if the buffer is empty
    thus avoiding relatively expensive throwing in this trivial case.

--- src/security/Handshake.cc
+++ src/security/Handshake.cc
@@ -606,23 +606,23 @@ bool
 Security::HandshakeParser::parseHello(const SBuf &data)
 {
     try {
-        if (!expectingModernRecords.configured())
-            expectingModernRecords.configure(!isSslv2Record(data));
+        if (!data.isEmpty()) {
+            if (!expectingModernRecords.configured())
+                expectingModernRecords.configure(!isSslv2Record(data));
 
-        // data contains everything read so far, but we may read more later
-        tkRecords.reinput(data, true);
-        tkRecords.rollback();
-        while (!done)
-            parseRecord();
-        debugs(83, 7, "success; got: " << done);
-        // we are done; tkRecords may have leftovers we are not interested in
-        return true;
+            // data contains everything read so far, but we may read more later
+            tkRecords.reinput(data, true);
+            tkRecords.rollback();
+            while (!done)
+                parseRecord();
+            debugs(83, 7, "success; got: " << done);
+            // we are done; tkRecords may have leftovers we are not interested in
+            return true;
+        }
     }
-    catch (const Parser::BinaryTokenizer::InsufficientInput &) {
-        debugs(83, 5, "need more data");
-        return false;
-    }
-    return false; // unreached
+    catch (const Parser::BinaryTokenizer::InsufficientInput &) {}
+    debugs(83, 5, "need more data");
+    return false;
 }
 
 /// Creates and returns a certificate by parsing a DER-encoded X509 structure.
