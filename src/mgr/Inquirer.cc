fc77a80df43c38301415b7b65b69e3f26cecd05e
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2020-06-26 16:05:22 +0300

    Undone an out-of-scope change

--- src/mgr/Inquirer.cc
+++ src/mgr/Inquirer.cc
@@ -117,12 +117,9 @@ Mgr::Inquirer::noteCommClosed(const CommCloseCbParams& params)
 bool
 Mgr::Inquirer::aggregate(Ipc::Response::Pointer aResponse)
 {
-    debugs(16, 5, HERE);
     Mgr::Response& response = static_cast<Response&>(*aResponse);
-    if (response.hasAction()) {
-        debugs(16, 5, aggrAction->name());
+    if (response.hasAction())
         aggrAction->add(response.getAction());
-    }
     return true;
 }
 
