measurement-factory/squid

commit 5d71dd4a63d506617d8a7ee39702bf62c837191d

Refactored and commented OneToOneQueue statistics methods

src/ipc/Queue.h
@@ -124,8 +124,13 @@ public:
 
 private:
     /// a common method for statIn() and statOut()
+    /// \param size the size of the queue at the moment of the call
+    /// \param startPos the absolute index (based on theIn or theOut) of the first available queue element
     template<class Value> void stat(StoreEntry &, const uint32_t size, const unsigned int startPos) const;
-    /// outputs several queue elements (for statistics) to the provided StoreEntry
+    /// outputs few queue elements (for statistics) to the provided StoreEntry
+    /// \param startPos the absolute index (based on theIn or theOut) of the first available queue element
+    /// \param offset skip this number of elements, starting from startPos
+    /// \param requested the number of elements to copy to StoreEntry
     template<class Value> void statElements(StoreEntry &, const unsigned int startPos, const uint32_t offset, const uint32_t requested) const;
 
     unsigned int theIn; ///< input index, used only in push()
@@ -452,17 +457,17 @@ OneToOneUniQueue::stat(StoreEntry &entry, const uint32_t aSize, const unsigned i
             aSize, theCapacity, theIn, theOut);
 
     if (!empty()) {
-        static const auto elementsNumber = 3;
-        auto n = elementsNumber < aSize ? elementsNumber : aSize;
-        statElements<Value>(entry, startPos, 0, n);
-        if (n < aSize) {
-            if (aSize > 2*n)
+        static const auto groupSize = 3;
+        // we output maximum two groups of elements, taken from the beginning and the end of the buffer
+        auto elementsInGroup = groupSize < aSize ? groupSize : aSize;
+        statElements<Value>(entry, startPos, 0, elementsInGroup);
+        if (elementsInGroup < aSize) { // else the first group consumed all queue elements
+            if (aSize > 2 * elementsInGroup) // else no delimiter since all buffer elements will be showed
                 storeAppendPrintf(&entry, "\t\t# ...\n");
-            // no overlapping with the previous elements
-            const auto offset = (aSize - n < n) ? n : aSize - n;
-            if (offset == n)
-                n = aSize - offset;
-            statElements<Value>(entry, startPos, offset, n);
+            const auto defaultOffset = aSize - elementsInGroup;
+            // no overlapping with the first group
+            const auto secondGroupOffset = defaultOffset < elementsInGroup ? elementsInGroup : defaultOffset;
+            statElements<Value>(entry, startPos, secondGroupOffset, elementsInGroup);
         }
     }
 }

commit be21947fd7ab6fdcf6e1d186ffeeedfdf5fb269e

Calculate the number of available elements for the 'second sample' group
... instead of using the constant 'sampleSize' (and printing trailing
zero elements in some cases). This problem was caused by 5d71dd4.

src/ipc/Queue.h
@@ -487,7 +487,8 @@ OneToOneUniQueue::statClose(StoreEntry &entry, const unsigned int start, const u
             // the last sampleSize items (i.e. starting at count-sampleSize
             // offset) except those already reported by the first sample.
             const auto secondSampleOffset = std::max(sampleSize, count - sampleSize);
-            statRange<Value>(entry, start, secondSampleOffset, sampleSize);
+            const auto secondSampleSize = std::min(sampleSize, count - sampleSize);
+            statRange<Value>(entry, start, secondSampleOffset, secondSampleSize);
         }
         storeAppendPrintf(&entry, "  ]");
     } else {