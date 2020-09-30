measurement-factory/squid

commit 7f98aad5dcd9f566b4668b9764024d71a95cbb25

Restore PURGE miss replies to be "404 Not Found", not "0 Init" (#586)
Since commit 6956579, PURGE requests resulted in invalid HTTP responses
with zero status code when Store lacked both GET and HEAD entries with
the requested URI.

Also adjusted Http::StatusLine packing code to avoid generating similar
invalid responses in the future (and to attract developer attention to
their presence in the code logic with a BUG message).

This is a Measurement Factory project.

src/client_side_reply.cc
@@ -1048,8 +1048,6 @@ void
 clientReplyContext::purgeDoPurgeGet(StoreEntry *newEntry)
 {
     if (newEntry) {
-        /* Move to new() when that is created */
-        purgeStatus = Http::scNotFound;
         /* Release the cached URI */
         debugs(88, 4, "clientPurgeRequest: GET '" << newEntry->url() << "'" );
 #if USE_HTCP
@@ -1102,6 +1100,9 @@ clientReplyContext::purgeDoPurgeHead(StoreEntry *newEntry)
             purgeStatus = Http::scOkay;
         }
     }
+
+    if (purgeStatus == Http::scNone)
+        purgeStatus = Http::scNotFound;
 
     /*
      * Make a new entry to hold the reply to be written

src/http/StatusLine.cc
@@ -47,6 +47,17 @@ Http::StatusLine::packInto(Packable * p) const
 {
     assert(p);
 
+    auto packedStatus = status();
+    auto packedReason = reason();
+
+    if (packedStatus == Http::scNone) {
+        static unsigned int reports = 0;
+        if (++reports <= 100)
+            debugs(57, DBG_IMPORTANT, "BUG: Generated response lacks status code");
+        packedStatus = Http::scInternalServerError;
+        packedReason = Http::StatusCodeString(packedStatus); // ignore custom reason_ (if any)
+    }
+
     /* local constants */
     /* AYJ: see bug 2469 - RFC2616 confirms stating 'SP characters' plural! */
     static const char *Http1StatusLineFormat = "HTTP/%d.%d %3d %s\r\n";
@@ -56,15 +67,15 @@ Http::StatusLine::packInto(Packable * p) const
     if (protocol == AnyP::PROTO_ICY) {
         debugs(57, 9, "packing sline " << this << " using " << p << ":");
         debugs(57, 9, "FORMAT=" << IcyStatusLineFormat );
-        debugs(57, 9, "ICY " << status() << " " << reason());
-        p->appendf(IcyStatusLineFormat, status(), reason());
+        debugs(57, 9, "ICY " << packedStatus << " " << packedReason);
+        p->appendf(IcyStatusLineFormat, packedStatus, packedReason);
         return;
     }
 
     debugs(57, 9, "packing sline " << this << " using " << p << ":");
     debugs(57, 9, "FORMAT=" << Http1StatusLineFormat );
-    debugs(57, 9, "HTTP/" << version.major << "." << version.minor << " " << status() << " " << reason());
-    p->appendf(Http1StatusLineFormat, version.major, version.minor, status(), reason());
+    debugs(57, 9, "HTTP/" << version.major << "." << version.minor << " " << packedStatus << " " << packedReason);
+    p->appendf(Http1StatusLineFormat, version.major, version.minor, packedStatus, packedReason);
 }
 
 /*

commit 18d80450bc9454e5b774074408c5515e63872a5d

Bug 5057: "BUG: Generated response lacks status code" for reply
The bug was caused by 7f98aad, which incorrectly assumed that only
self-generated request could have Http::scNone. As a result, replies
with HTTP code 0 led to misleading BUG message.  Now the BUG message is
printed only the PURGE code failed to set purgeStatus before response
generation.

src/client_side_reply.cc
@@ -1048,18 +1048,24 @@ clientReplyContext::purgeDoMissPurge()
 void
 clientReplyContext::purgeDoPurgeGet(StoreEntry *newEntry)
 {
-    if (newEntry) {
-        /* Release the cached URI */
-        debugs(88, 4, "clientPurgeRequest: GET '" << newEntry->url() << "'" );
-#if USE_HTCP
-        neighborsHtcpClear(newEntry, http->request, HttpRequestMethod(Http::METHOD_GET), HTCP_CLR_PURGE);
-#endif
-        newEntry->release(true);
-        purgeStatus = Http::scOkay;
-    }
-
+    purgeEntry(newEntry, Http::METHOD_GET);
     lookingforstore = 4;
     StoreEntry::getPublicByRequestMethod(this, http->request, Http::METHOD_HEAD);
+}
+
+void
+clientReplyContext::purgeEntry(StoreEntry *entry, const Http::MethodType methodType, const char *description)
+{
+    if (!entry) {
+        purgeStatus = Http::scNotFound;
+        return;
+    }
+    debugs(88, 4, description << " " << Http::MethodStr(methodType) << " '" << entry->url() << "'" );
+#if USE_HTCP
+    neighborsHtcpClear(entry, http->request, HttpRequestMethod(methodType), HTCP_CLR_PURGE);
+#endif
+    entry->release(true);
+    purgeStatus = Http::scOkay;
 }
 
 void
@@ -1067,11 +1073,7 @@ clientReplyContext::purgeDoPurgeHead(StoreEntry *newEntry)
 {
     if (newEntry) {
         debugs(88, 4, "HEAD " << newEntry->url());
-#if USE_HTCP
-        neighborsHtcpClear(newEntry, http->request, HttpRequestMethod(Http::METHOD_HEAD), HTCP_CLR_PURGE);
-#endif
-        newEntry->release(true);
-        purgeStatus = Http::scOkay;
+        purgeEntry(newEntry, Http::METHOD_HEAD);
     }
 
     /* And for Vary, release the base URI if none of the headers was included in the request */
@@ -1079,31 +1081,19 @@ clientReplyContext::purgeDoPurgeHead(StoreEntry *newEntry)
             && http->request->vary_headers.find('=') != SBuf::npos) {
         // XXX: performance regression, c_str() reallocates
         SBuf tmp(http->request->effectiveRequestUri());
-        StoreEntry *entry = storeGetPublic(tmp.c_str(), Http::METHOD_GET);
 
-        if (entry) {
-            debugs(88, 4, "Vary GET " << entry->url());
-#if USE_HTCP
-            neighborsHtcpClear(entry, http->request, HttpRequestMethod(Http::METHOD_GET), HTCP_CLR_PURGE);
-#endif
-            entry->release(true);
-            purgeStatus = Http::scOkay;
-        }
+        StoreEntry *entry = storeGetPublic(tmp.c_str(), Http::METHOD_GET);
+        purgeEntry(entry, Http::METHOD_GET, "Vary");
 
         entry = storeGetPublic(tmp.c_str(), Http::METHOD_HEAD);
-
-        if (entry) {
-            debugs(88, 4, "Vary HEAD " << entry->url());
-#if USE_HTCP
-            neighborsHtcpClear(entry, http->request, HttpRequestMethod(Http::METHOD_HEAD), HTCP_CLR_PURGE);
-#endif
-            entry->release(true);
-            purgeStatus = Http::scOkay;
-        }
+        purgeEntry(entry, Http::METHOD_HEAD, "Vary");
     }
 
-    if (purgeStatus == Http::scNone)
-        purgeStatus = Http::scNotFound;
+    if (purgeStatus == Http::scNone) {
+        static unsigned int reports = 0;
+        if (++reports <= 100)
+            debugs(57, DBG_IMPORTANT, "BUG: Generated PURGE response lacks status code");
+    }
 
     /*
      * Make a new entry to hold the reply to be written

src/client_side_reply.h
@@ -127,6 +127,8 @@ private:
     void triggerInitialStoreRead();
     void sendClientOldEntry();
     void purgeAllCached();
+    /// releases the cached entry
+    void purgeEntry(StoreEntry *, const Http::MethodType, const char *description = nullptr);
     void forgetHit();
     bool blockedHit() const;
     void detailStoreLookup(const char *detail);

src/http/StatusLine.cc
@@ -51,9 +51,6 @@ Http::StatusLine::packInto(Packable * p) const
     auto packedReason = reason();
 
     if (packedStatus == Http::scNone) {
-        static unsigned int reports = 0;
-        if (++reports <= 100)
-            debugs(57, DBG_IMPORTANT, "BUG: Generated response lacks status code");
         packedStatus = Http::scInternalServerError;
         packedReason = Http::StatusCodeString(packedStatus); // ignore custom reason_ (if any)
     }