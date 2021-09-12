f04e1003f85a6256a12d61798b4f2d63e0d77721
Alex Rousskov <rousskov@measurement-factory.com>
2021-09-12 18:13:33 -0400

    Stop (ab)using Transient entry flags for Collapsing Requirement mngmt
    The ENTRY_REQUIRES_COLLAPSING flag was used for relaying "hitting this
    entry carries CF risks right now" info to other workers. That flag is
    not necessary to relay that info because remote workers already know
    whether the entry is attached to one of the shared cache stores.
    Moreover, relaying that flag correctly is difficult and would require a
    different implementation:
    
    * The flag is changed in relatively high-level Store code, possibly
      _after_ we stopped writing, when the transients entry is already
      read-only. This bug leads to an isWriter() assertion in
      Transients::clearCollapsingRequirement() called via a quick or aborted
      StoreEntry::startWriting().
    
    * Basics.flags is not an atomic field. It lacks an "[app]" mark -- its
      readers will not get a reliable value due to race conditions when
      Squid will need to update the Transient entry flags in "append" mode.
    
    * The flag is changed assuming that simply sending stuff to disk is
      sufficient for other workers to read it. This will soon become
      inaccurate because the store index will be updated only after the slot
      is written to disk.
    
    Remote workers now use Store attachment info to set the local StoreEntry
    object flag, ignoring the no-longer-used Transients entry flags.
    
    The ENTRY_REQUIRES_COLLAPSING flag is still used to tell transactions
    which StoreEntry objects are subject to CF risks. It would be nice to
    use the presence of MemObject::reply_ to relay the same info for
    store-unattached entries (and attachment info for others), but we could
    not find a way to do that (during commit d2a6dcb work and then again
    during this project) without a lot of essentially out of scope rewrites.
    
    Also removed misleading Ipc::StoreMapAnchor::exportInto() debugging.
    
    Cherry-picked SQUID-448-cf-without-transients-flags-bag51
    commits 1ee4829..d8dad48.

--- src/store/Controller.cc
+++ src/store/Controller.cc
@@ -655,13 +655,6 @@ Store::Controller::transientsDisconnect(StoreEntry &e)
 }
 
 void
-Store::Controller::transientsClearCollapsingRequirement(StoreEntry &e)
-{
-    if (transients)
-        transients->clearCollapsingRequirement(e);
-}
-
-void
 Store::Controller::handleIdleEntry(StoreEntry &e)
 {
     bool keepInLocalMemory = false;
@@ -807,11 +800,6 @@ Store::Controller::syncCollapsed(const sfileno xitIndex)
     Transients::EntryStatus entryStatus;
     transients->status(*collapsed, entryStatus);
 
-    if (!entryStatus.collapsed) {
-        debugs(20, 5, "removing collapsing requirement for " << *collapsed << " since remote writer probably got headers");
-        collapsed->setCollapsingRequirement(false);
-    }
-
     if (entryStatus.waitingToBeFreed) {
         debugs(20, 3, "will release " << *collapsed << " due to waitingToBeFreed");
         collapsed->release(true); // may already be marked
@@ -821,12 +809,6 @@ Store::Controller::syncCollapsed(const sfileno xitIndex)
         return; // readers can only change our waitingToBeFreed flag
 
     assert(transients->isReader(*collapsed));
-
-    if (entryStatus.collapsed && !collapsed->hittingRequiresCollapsing()) {
-        debugs(20, 3, "aborting " << *collapsed << " due to writer/reader collapsing state mismatch");
-        collapsed->abort();
-        return;
-    }
 
     bool found = false;
     bool inSync = false;
@@ -854,6 +836,8 @@ Store::Controller::syncCollapsed(const sfileno xitIndex)
 
     if (inSync) {
         debugs(20, 5, "synced " << *collapsed);
+        assert(found);
+        collapsed->setCollapsingRequirement(false);
         collapsed->invokeHandlers();
         return;
     }
@@ -872,6 +856,7 @@ Store::Controller::syncCollapsed(const sfileno xitIndex)
 
     // the entry is still not in one of the caches
     debugs(20, 7, "waiting " << *collapsed);
+    collapsed->setCollapsingRequirement(true);
 }
 
 /// Called for Transients entries that are not yet anchored to a cache.
@@ -889,33 +874,37 @@ Store::Controller::anchorToCache(StoreEntry &entry, bool &inSync)
     Transients::EntryStatus entryStatus;
     transients->status(entry, entryStatus);
 
+    inSync = false;
     bool found = false;
     if (sharedMemStore)
         found = sharedMemStore->anchorToCache(entry, inSync);
     if (!found && swapDir)
         found = swapDir->anchorToCache(entry, inSync);
 
+    if (inSync) {
+        debugs(20, 7, "anchored " << entry);
+        assert(found);
+        entry.setCollapsingRequirement(false);
+        return true;
+    }
+
     if (found) {
-        if (inSync)
-            debugs(20, 7, "anchored " << entry);
-        else
-            debugs(20, 5, "failed to anchor " << entry);
+        debugs(20, 5, "failed to anchor " << entry);
         return true;
     }
 
     if (entryStatus.waitingToBeFreed) {
         debugs(20, 5, "failed on marked unattached " << entry);
-        inSync = false;
         return true;
     }
 
     if (!entryStatus.hasWriter) {
         debugs(20, 5, "failed on abandoned-by-writer " << entry);
-        inSync = false;
         return true;
     }
 
     debugs(20, 7, "skipping not yet cached " << entry);
+    entry.setCollapsingRequirement(true);
     return false;
 }
 
