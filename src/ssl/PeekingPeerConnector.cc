35b73fa3d2c077816ca96c693c25661232015361
Alex Rousskov <rousskov@measurement-factory.com>
2021-09-29 12:36:39 -0400

    PeekingPeerConnector: Check the step once; do not quit if cannot check!
    PeekingPeerConnector should only operate during step3, so we should
    check the step when the job is created. If we ever add step4, it might
    make sense to check that we are still at step3 while the job runs, but
    that also depends on what step4 will be (and where it will be changed).
    
    We could even check earlier, in FwdState constructor because no server
    communication should happen in earlier steps. TODO?

--- src/ssl/PeekingPeerConnector.cc
+++ src/ssl/PeekingPeerConnector.cc
@@ -26,6 +26,28 @@ CBDATA_NAMESPACED_CLASS_INIT(Ssl, PeekingPeerConnector);
 
 void switchToTunnel(HttpRequest *request, const Comm::ConnectionPointer &clientConn, const Comm::ConnectionPointer &srvConn, const SBuf &preReadServerData);
 
+Ssl::PeekingPeerConnector::PeekingPeerConnector(HttpRequestPointer &aRequest,
+        const Comm::ConnectionPointer &aServerConn,
+        const Comm::ConnectionPointer &aClientConn,
+        AsyncCall::Pointer &aCallback,
+        const AccessLogEntryPointer &alp,
+        const time_t timeout):
+    AsyncJob("Ssl::PeekingPeerConnector"),
+    Security::PeerConnector(aServerConn, aCallback, alp, timeout),
+    clientConn(aClientConn),
+    splice(false),
+    serverCertificateHandled(false)
+{
+    request = aRequest;
+
+    if (const auto csd = request->clientConnectionManager.valid()) {
+        const auto serverBump = csd->serverBump();
+        Must(serverBump);
+        Must(serverBump->at(XactionStep::tlsBump3));
+    }
+    // else the client is gone, and we cannot check the step, but must carry on
+}
+
 void
 Ssl::PeekingPeerConnector::cbCheckForPeekAndSpliceDone(Acl::Answer answer, void *data)
 {
@@ -46,12 +68,6 @@ Ssl::PeekingPeerConnector::checkForPeekAndSpliceDone(Acl::Answer answer)
 void
 Ssl::PeekingPeerConnector::checkForPeekAndSplice()
 {
-    if (!request->clientConnectionManager)
-        return;
-    const auto serverBump = request->clientConnectionManager->serverBump();
-    Must(serverBump);
-    Must(serverBump->at(XactionStep::tlsBump3));
-
     handleServerCertificate();
 
     ACLFilledChecklist *acl_checklist = new ACLFilledChecklist(
@@ -169,8 +185,6 @@ Ssl::PeekingPeerConnector::initialize(Security::SessionPointer &serverSession)
         if (hostName)
             SSL_set_ex_data(serverSession.get(), ssl_ex_index_server, (void*)hostName);
 
-        Must(csd->serverBump());
-        Must(csd->serverBump()->at(XactionStep::tlsBump3));
         if (csd->sslBumpMode == Ssl::bumpPeek || csd->sslBumpMode == Ssl::bumpStare) {
             auto clientSession = fd_table[clientConn->fd].ssl.get();
             Must(clientSession);
