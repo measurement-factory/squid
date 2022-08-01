88e06162042024556f55000da9999d3d419b8a47
Alex Rousskov <rousskov@measurement-factory.com>
2022-08-01 17:25:10 -0400

    Avoid sslcrtd_program db corruption due to initialization conflicts
    Ssl::CertificateDb::Check() reads the database and, if the db size is
    corrupted rebuilds that file. Both operations are susceptible to
    Check()-Check() and Check()-update conflicts among the helpers. Thus,
    Check() must lock the environment for exclusive use.

--- src/security/cert_generators/file/certificate_db.cc
+++ src/security/cert_generators/file/certificate_db.cc
@@ -394,6 +394,7 @@ Ssl::CertificateDb::Create(std::string const & db_path) {
 void
 Ssl::CertificateDb::Check(std::string const & db_path, size_t max_db_size, size_t fs_block_size) {
     CertificateDb db(db_path, max_db_size, fs_block_size);
+    const Locker locker(db.dbLock, Here);
     db.load();
 
     // Call readSize to force rebuild size file in the case it is corrupted
