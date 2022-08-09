89361b65aa61ac0b05b7ca6630e6c19147460572
Amos Jeffries <yadij@users.noreply.github.com>
2022-08-09 21:23:07 +0000

    Remove send-announce
    This is another feature which is very much outdated and unused.
    
    ----
    
    Cherry-picked master/v6 commit a6e452cdf7238990f058bfaf3245111ec13b7486
    and resolved simple conflicts in src/cf.data.pre, src/main.cc, and
    src/send-announce.cc.

--- src/main.cc
+++ src/main.cc
@@ -70,7 +70,6 @@
 #include "refresh.h"
 #include "sbuf/Stream.h"
 #include "SBufStatsAction.h"
-#include "send-announce.h"
 #include "SquidConfig.h"
 #include "SquidTime.h"
 #include "stat.h"
@@ -1022,14 +1021,6 @@ mainReconfigureFinish(void *)
     Config.ClientDelay.finalize();
 #endif
 
-    if (Config.onoff.announce) {
-        if (!eventFind(start_announce, NULL))
-            eventAdd("start_announce", start_announce, NULL, 3600.0, 1);
-    } else {
-        if (eventFind(start_announce, NULL))
-            eventDelete(start_announce, NULL);
-    }
-
     reconfiguring = 0;
 }
 
@@ -1333,9 +1324,6 @@ mainInitialize(void)
 #endif
 
     eventAdd("storeMaintain", Store::Maintain, nullptr, 1.0, 1);
-
-    if (Config.onoff.announce)
-        eventAdd("start_announce", start_announce, nullptr, 3600.0, 1);
 
     eventAdd("ipcache_purgelru", ipcache_purgelru, nullptr, 10.0, 1);
 
