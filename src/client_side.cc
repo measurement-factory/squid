e403a1e9004e5d4c91996830f2ac9149696ed42f
Alex Rousskov <rousskov@measurement-factory.com>
2022-08-19 18:57:50 +0000

    Honor httpd_suppress_version_string in more contexts
    * HTML "signature" part of generated HTTP responses to URN requests
    * Server header in generated HTTP redirect responses
    * boundary strings in generated HTTP 206 multipart/byteranges responses
    
    Also document known contexts outside the configuration directive reach.
    We may assert that all of them are outside the current "httpd" scope
    (implied by the directive name), but admins may not know how we define
    that "httpd" scope; being more explicit may reduce surprises.
    
    ----
    
    Cherry-picked master/v6 commit c81de627914ae4a943fbedc10e880555b508a059.

--- src/client_side.cc
+++ src/client_side.cc
@@ -824,7 +824,7 @@ String
 ClientHttpRequest::rangeBoundaryStr() const
 {
     const char *key;
-    String b(APP_FULLNAME);
+    String b(visible_appname_string);
     b.append(":",1);
     key = storeEntry()->getMD5Text();
     b.append(key, strlen(key));
