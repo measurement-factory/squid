7fdabf9028f49d9147e1132c369353156cc8ecc9
Alex Rousskov <rousskov@measurement-factory.com>
2023-04-30 17:40:14 -0400

    Removed unused StoreIOState::STFNCB callbacks
    Unused since before 2016 commit abf396e that discovered the problem.
    
    ----
    
    Cherry-picked master/v6 commit be42c788f07a838da475bec9c6f7409f1d4e3478
    and resolved trivial virtual/override conflicts (missing 337b9aa).

--- src/tests/TestSwapDir.h
+++ src/tests/TestSwapDir.h
@@ -30,8 +30,8 @@ public:
     virtual void init() override;
     virtual bool unlinkdUseful() const override;
     virtual bool canStore(const StoreEntry &e, int64_t diskSpaceNeeded, int &load) const override;
-    virtual StoreIOState::Pointer createStoreIO(StoreEntry &, StoreIOState::STFNCB *, StoreIOState::STIOCB *, void *) override;
-    virtual StoreIOState::Pointer openStoreIO(StoreEntry &, StoreIOState::STFNCB *, StoreIOState::STIOCB *, void *) override;
+    virtual StoreIOState::Pointer createStoreIO(StoreEntry &, StoreIOState::STIOCB *, void *) override;
+    virtual StoreIOState::Pointer openStoreIO(StoreEntry &, StoreIOState::STIOCB *, void *) override;
     virtual void parse(int, char*) override;
     virtual void evictCached(StoreEntry &) override {}
     virtual void evictIfFound(const cache_key *) override {}
