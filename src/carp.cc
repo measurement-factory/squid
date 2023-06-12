f36c53e0350f0a191cbd4c3194cb272d4141e24e
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2023-06-12 17:37:10 +0300

    Fixed carp Cache Manager report
    which incorrectly reported about all Config.peers instead
    of just carp_peers.

--- src/carp.cc
+++ src/carp.cc
@@ -203,7 +203,6 @@ carpSelectParent(PeerSelector *ps)
 static void
 carpCachemgr(StoreEntry * sentry)
 {
-    CachePeer *p;
     int sumfetches = 0;
     storeAppendPrintf(sentry, "%24s %10s %10s %10s %10s\n",
                       "Hostname",
@@ -212,10 +211,10 @@ carpCachemgr(StoreEntry * sentry)
                       "Factor",
                       "Actual");
 
-    for (p = Config.peers; p; p = p->next)
+    for (const auto &p: CarpPeers)
         sumfetches += p->stats.fetches;
 
-    for (p = Config.peers; p; p = p->next) {
+    for (const auto &p: CarpPeers) {
         storeAppendPrintf(sentry, "%24s %10x %10f %10f %10f\n",
                           p->name, p->carp.hash,
                           p->carp.load_multiplier,
