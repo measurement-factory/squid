33d14b2ca9cc340bc1463a58898ed8dea2e66509
Alex Rousskov <rousskov@measurement-factory.com>
2023-07-28 21:08:11 -0400

    fixup: Simplify by skipping ssl_bump in no-serverBump transactions
    This change assumes that we will make sure that serverBump is set for
    and only for ConnStateData objects that are (or were) subject to
    ssl_bump processing.
    
    ----
    
    Cherry-picked bag62 commit bde261a.

--- src/client_side_request.cc
+++ src/client_side_request.cc
@@ -1350,12 +1350,12 @@ ClientRequestContext::sslBumpAccessCheck()
         return false;
     }
 
-    auto srvBump = http->getConn()->serverBump();
+    const auto srvBump = http->getConn()->serverBump();
+    if (!srvBump) {
+        debugs(85, 5, "not currently subject to ssl_bump: " << http->getConn());
+        return false;
+    }
 
-    if (srvBump && srvBump->at(XactionStep::tlsBump2)) {
-        assert(http->request->method == Http::METHOD_CONNECT);
-
-    } else {
     const Ssl::BumpMode bumpMode = http->getConn()->sslBumpMode;
     if (http->request->flags.forceTunnel) {
         debugs(85, 5, "not needed; already decided to tunnel " << http->getConn());
@@ -1392,7 +1392,6 @@ ClientRequestContext::sslBumpAccessCheck()
         debugs(85, 5, "no SslBump during proxy authentication");
         return false;
     }
-    }
 
     if (error) {
         debugs(85, 5, "SslBump applies. Force bump action on error " << errorTypeName(error->type));
@@ -1404,7 +1403,8 @@ ClientRequestContext::sslBumpAccessCheck()
     debugs(85, 5, "SslBump possible, checking ACL");
 
     ACLFilledChecklist *aclChecklist = clientAclChecklistCreate(Config.accessList.ssl_bump, http);
-    if (srvBump && srvBump->at(XactionStep::tlsBump2)) {
+
+    if (srvBump->at(XactionStep::tlsBump2)) {
         aclChecklist->banAction(Acl::Answer(ACCESS_ALLOWED, Ssl::bumpNone));
         aclChecklist->banAction(Acl::Answer(ACCESS_ALLOWED, Ssl::bumpClientFirst));
         aclChecklist->banAction(Acl::Answer(ACCESS_ALLOWED, Ssl::bumpServerFirst));
