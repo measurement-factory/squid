33798fae0311f9c9ea7611d82db0345b4f3257c7
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2024-03-23 21:24:03 +0000

    HTTP: Protect just-parsed responses from accidental destruction
    The lack of HttpReply locking meant that we could not (safely) pass the
    freshly created reply object to ACLFilledChecklist because the checklist
    destructor would unwittingly destroy the object (by unlocking it). There
    is at least one applicable ACL check in handle1xx(), but we got lucky
    because that method adds the reply object to ALE, increasing its
    reference counter. This change stops relying on such "external" locks.
    
    This change also protects from HttpReply memory leaks when an exception
    is thrown between HttpReply creation and a setVirginReply() call.
    
    TODO: Fix all cases where newly created HttpReply objects are remembered
    using raw pointers (but may be passed to locking/unlocking code).
    
    ----
    
    Cherry-picked master/v7 commit 80a6a08a94a8c1029b907169e5803fe2e5e9a422.

--- src/http.cc
+++ src/http.cc
@@ -705,7 +705,7 @@ HttpStateData::processReplyHeader()
     // reset payload tracking to begin after message headers
     payloadSeen = inBuf.length();
 
-    HttpReplyPointer newrep = new HttpReply;
+    const auto newrep = HttpReply::Pointer::Make();
     // XXX: RFC 7230 indicates we MAY ignore the reason phrase,
     //      and use an empty string on unknown status.
     //      We do that now to avoid performance regression from using SBuf::c_str()
@@ -740,7 +740,7 @@ HttpStateData::processReplyHeader()
     if (!peerSupportsConnectionPinning())
         request->flags.connectionAuthDisabled = true;
 
-    HttpReply *vrep = setVirginReply(newrep.getRaw());
+    const auto vrep = setVirginReply(newrep.getRaw());
     flags.headers_parsed = true;
 
     keepaliveAccounting(vrep);
@@ -752,12 +752,10 @@ HttpStateData::processReplyHeader()
 
 /// ignore or start forwarding the 1xx response (a.k.a., control message)
 void
-HttpStateData::handle1xx(HttpReply *reply)
+HttpStateData::handle1xx(const HttpReply::Pointer &reply)
 {
     if (fwd->al)
         fwd->al->reply = reply;
-
-    HttpReply::Pointer msg(reply); // will destroy reply if unused
 
     // one 1xx at a time: we must not be called while waiting for previous 1xx
     Must(!flags.handling1xx);
@@ -781,7 +779,7 @@ HttpStateData::handle1xx(HttpReply *reply)
     if (Config.accessList.reply) {
         ACLFilledChecklist ch(Config.accessList.reply, originalRequest().getRaw());
         ch.al = fwd->al;
-        ch.reply = reply;
+        ch.reply = reply.getRaw();
         ch.syncAle(originalRequest().getRaw(), nullptr);
         HTTPMSGLOCK(ch.reply);
         if (!ch.fastCheck().allowed()) // TODO: support slow lookups?
@@ -801,7 +799,7 @@ HttpStateData::handle1xx(HttpReply *reply)
     const AsyncCall::Pointer cb = JobCallback(11, 3, CbDialer, this,
                                   HttpStateData::proceedAfter1xx);
     CallJobHere1(11, 4, request->clientConnectionManager, ConnStateData,
-                 ConnStateData::sendControlMsg, HttpControlMsg(msg, cb));
+                 ConnStateData::sendControlMsg, HttpControlMsg(reply, cb));
     // If the call is not fired, then the Sink is gone, and HttpStateData
     // will terminate due to an aborted store entry or another similar error.
     // If we get stuck, it is not handle1xx fault if we could get stuck
