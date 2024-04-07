0f0ddd984762978054a3e46ff46dd95a5a09f3fd
Alex Rousskov <rousskov@measurement-factory.com>
2024-04-07 14:21:21 -0400

    Populate transaction annotations with eCAP meta headers
    ACL "note" documentation describes several sources of transaction
    annotations, including "eCAP responses". However, eCAP annotations were
    not accessible via the "note" ACL and %note logformat code. We now copy
    eCAP transaction options (a.k.a. eCAP meta headers) into the existing
    Adaptation::History::metaHeaders field that was meant for storing them.
    
    Also create adaptation history object if adaptation_meta is configured.
    Before this fix, the adaptation history object creation depended only on
    Log::TheConfig.hasAdaptToken flag: if the flag was false, the history
    object was not created and, hence, eCAP/ICAP code could not add
    adaptation_meta headers into it. As a result, transaction annotation
    lacked these headers and 'note acl' could not match them.
    
    TODO: Fix ICAP as well. Polish.
    
    ----
    
    Cherry-picked SQUID-409-note-does-not-see-ecap-annotations commits
    aa35742b..f22a19af and resolved trivial (mostly NULL/nullptr) conflicts.

--- src/HttpRequest.cc
+++ src/HttpRequest.cc
@@ -32,6 +32,10 @@
 #include "SquidConfig.h"
 #include "Store.h"
 
+#if USE_ADAPTATION
+#include "adaptation/Config.h"
+#endif
+
 #if USE_AUTH
 #include "auth/UserRequest.h"
 #endif
@@ -414,7 +418,10 @@ HttpRequest::adaptHistory(bool createIfNone) const
 Adaptation::History::Pointer
 HttpRequest::adaptLogHistory() const
 {
-    return HttpRequest::adaptHistory(Log::TheConfig.hasAdaptToken);
+    // TODO: we also need the history for dynamically loaded error pages, using logformat %code.
+    // So we need either modifying the condition to take into account this case or creating the
+    // history unconditionally.
+    return HttpRequest::adaptHistory(Log::TheConfig.hasAdaptToken || !Adaptation::Config::metaHeaders.empty());
 }
 
 void
