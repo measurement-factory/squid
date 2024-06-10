measurement-factory/squid

commit cc71dd7cd757c77c6c9df72a4c60b2d6209def96

annotate_client and annotate_transaction ACLs must always match (#1820)
    WARNING: markAsTunneled ACL is used in context without an HTTP
    request. Assuming mismatch.

Our annotate_client and annotate_transaction ACLs are documented as
"always matching", and some existing Squid configurations rely on that
invariant. Both ACLs did not match when they lacked access to the
current transaction information because their requiresRequest() method
returned true; annotate_client ACL also did not match after the
client-to-Squid connection was gone and when the transaction was not
associated with a client-to-Squid connection.

This change makes ACL code conform to documentation. Squid still warns
the admin if an ACL cannot annotate. Such warnings may indicate s Squid
bug or misconfiguration, but mismatching in those cases causes more harm
because it makes it impossible for the admin to rely on the primary
matching invariant. "One reliable invariant plus one unreliable side
effect" is the lesser evil than "two unreliable effects".

    # the following must deny even if it cannot annotate
    http_access deny markAsDenied

    # the following might not log denied traffic (with a prior warning)
    access_log syslog:daemon.err markedAsDenied

Also fixed annotate_client to annotate the current transaction even
after ConnStateData destruction. Such annotations may happen when, for
example, Squid continues a large download after the HTTP client is gone.

src/acl/AnnotateClient.cc
@@ -17,15 +17,29 @@ int
 Acl::AnnotateClientCheck::match(ACLChecklist * const ch)
 {
     const auto checklist = Filled(ch);
+    auto annotated = false;
+    const auto tdata = dynamic_cast<ACLAnnotationData*>(data.get());
+    assert(tdata);
+    const auto conn = checklist->conn();
 
-    if (const auto conn = checklist->conn()) {
-        const auto tdata = dynamic_cast<ACLAnnotationData*>(data.get());
-        assert(tdata);
+    if (conn) {
         tdata->annotate(conn->notes(), &delimiters.value, checklist->al);
-        if (const auto request = checklist->request)
-            tdata->annotate(request->notes(), &delimiters.value, checklist->al);
-        return 1;
+        annotated = true;
     }
-    return 0;
+
+    if (const auto &request = checklist->request) {
+        tdata->annotate(request->notes(), &delimiters.value, checklist->al);
+        annotated = true;
+    } else if (conn && !conn->pipeline.empty()) {
+        debugs(28, DBG_IMPORTANT, "ERROR: Squid BUG: " << name << " ACL is used in context with " <<
+               "an unexpectedly nil ACLFilledChecklist::request. Did not annotate the current transaction.");
+    }
+
+    if (!annotated) {
+        debugs(28, DBG_IMPORTANT, "WARNING: " << name << " ACL is used in context without " <<
+               "active client-to-Squid connection and current transaction information. Did not annotate.");
+    }
+
+    return 1; // this is an "always matching" ACL
 }
 

src/acl/AnnotateClient.h
@@ -20,7 +20,6 @@ class AnnotateClientCheck: public Acl::AnnotationCheck
 public:
     /* Acl::Node API */
     int match(ACLChecklist *) override;
-    bool requiresRequest() const override { return true; }
 };
 
 } // namespace Acl

src/acl/AnnotateTransaction.cc
@@ -21,8 +21,10 @@ Acl::AnnotateTransactionCheck::match(ACLChecklist * const ch)
         const auto tdata = dynamic_cast<ACLAnnotationData*>(data.get());
         assert(tdata);
         tdata->annotate(request->notes(), &delimiters.value, checklist->al);
-        return 1;
+    } else {
+        debugs(28, DBG_IMPORTANT, "WARNING: " << name << " ACL is used in context without " <<
+               "current transaction information. Did not annotate.");
     }
-    return 0;
+    return 1; // this is an "always matching" ACL
 }
 

src/acl/AnnotateTransaction.h
@@ -20,7 +20,6 @@ class AnnotateTransactionCheck: public Acl::AnnotationCheck
 public:
     /* Acl::Node API */
     int match(ACLChecklist *) override;
-    bool requiresRequest() const override { return true; }
 };
 
 } // namespace Acl

commit ba02aafd6ea4d97394abc2c2e64b308f7bb86cb7

annotate_client did not annotate error:* transactions
cc71dd7 introduced an admin-level BUG requirement that a non-empty
message pipeline means an existing HttpRequest object. That requirement
was incorrect because when the pipeline receives the HttpStream object
(returned by ConnStateData::parseOneRequest()), the corresponding
HttpRequest is not created yet (an may be not created at all, depending
on further checks). Now, instead of demanding HttpRequest
presence in annotate_client, we annotate requests if/when they get
created (and imported into ClientHttpRequest) by copying the existing
connection annotation. That copying already worked for successfully
parsed and fake HttpRequests, but was missing for error-uri requests
(such as "error:invalid-request").

Also fill these 'error:*' HttpRequests with connection-level
information. For example, access_log takes this information from
the request, and fails to match some ACLs, such as 'src'.

src/HttpRequest.cc
@@ -773,10 +773,18 @@ UpdateRequestNotes(ConnStateData *csd, HttpRequest &request, NotePairs const &he
 void
 HttpRequest::manager(const CbcPointer<ConnStateData> &aMgr, const AccessLogEntryPointer &al)
 {
+    if (clientConnectionManager == aMgr)
+        return;
+
     clientConnectionManager = aMgr;
 
     if (!clientConnectionManager.valid())
         return;
+
+    // TODO: We fill request notes here until we find a way to verify whether
+    // no ACL checking is performed before ClientHttpRequest::doCallouts().
+    if (clientConnectionManager->hasNotes())
+        notes()->appendNewOnly(clientConnectionManager->notes().getRaw());
 
     AnyP::PortCfgPointer port = clientConnectionManager->port;
     if (port) {

src/acl/AnnotateClient.cc
@@ -30,9 +30,6 @@ Acl::AnnotateClientCheck::match(ACLChecklist * const ch)
     if (const auto &request = checklist->request) {
         tdata->annotate(request->notes(), &delimiters.value, checklist->al);
         annotated = true;
-    } else if (conn && !conn->pipeline.empty()) {
-        debugs(28, DBG_IMPORTANT, "ERROR: Squid BUG: " << name << " ACL is used in context with " <<
-               "an unexpectedly nil ACLFilledChecklist::request. Did not annotate the current transaction.");
     }
 
     if (!annotated) {

src/client_side.cc
@@ -1578,8 +1578,6 @@ clientProcessRequest(ConnStateData *conn, const Http1::RequestParserPointer &hp,
     // this entire function to remove them from the FTP code path. Connection
     // setup and body_pipe preparation blobs are needed for FTP.
 
-    request->manager(conn, http->al);
-
     request->flags.accelerated = http->flags.accel;
     request->flags.sslBumped=conn->switchedToHttps();
     // TODO: decouple http->flags.accel from request->flags.sslBumped

src/client_side_request.cc
@@ -1612,10 +1612,6 @@ void
 ClientHttpRequest::initRequest(HttpRequest *aRequest)
 {
     assignRequest(aRequest);
-    if (const auto csd = getConn()) {
-        if (!csd->notes()->empty())
-            request->notes()->appendNewOnly(csd->notes().getRaw());
-    }
     // al is created in the constructor
     assert(al);
     if (!al->request) {

src/servers/FtpServer.cc
@@ -756,6 +756,7 @@ Ftp::Server::parseOneRequest()
     http->req_sz = tok.parsedSize();
     http->uri = newUri;
     http->initRequest(request);
+    request->manager(this, http->al);
 
     Http::Stream *const result =
         new Http::Stream(clientConnection, http);

src/servers/Http1Server.cc
@@ -93,7 +93,6 @@ void clientProcessRequestFinished(ConnStateData *conn, const HttpRequest::Pointe
 bool
 Http::One::Server::buildHttpRequest(Http::StreamPointer &context)
 {
-    HttpRequest::Pointer request;
     ClientHttpRequest *http = context->http;
     if (context->flags.parsed_ok == 0) {
         debugs(33, 2, "Invalid Request");
@@ -122,11 +121,8 @@ Http::One::Server::buildHttpRequest(Http::StreamPointer &context)
         assert(http->log_uri);
 
         const char * requestErrorBytes = inBuf.c_str();
-        if (!tunnelOnError(errPage)) {
-            setReplyError(context, request, errPage, parser_->parseStatusCode, requestErrorBytes);
-            // HttpRequest object not build yet, there is no reason to call
-            // clientProcessRequestFinished method
-        }
+        if (!tunnelOnError(errPage))
+            setReplyError(context, errPage, parser_->parseStatusCode, requestErrorBytes);
 
         return false;
     }
@@ -134,18 +130,15 @@ Http::One::Server::buildHttpRequest(Http::StreamPointer &context)
     // TODO: move URL parse into Http Parser and INVALID_URL into the above parse error handling
     const auto mx = MasterXaction::MakePortful(port);
     mx->tcpClient = clientConnection;
-    request = HttpRequest::FromUrlXXX(http->uri, mx, parser_->method());
+    HttpRequest::Pointer request = HttpRequest::FromUrlXXX(http->uri, mx, parser_->method());
     if (!request) {
         debugs(33, 5, "Invalid URL: " << http->uri);
         // setReplyToError() requires log_uri
         http->setLogUriToRawUri(http->uri, parser_->method());
 
         const char * requestErrorBytes = inBuf.c_str();
-        if (!tunnelOnError(ERR_INVALID_URL)) {
-            setReplyError(context, request, ERR_INVALID_URL, Http::scBadRequest, requestErrorBytes);
-            // HttpRequest object not build yet, there is no reason to call
-            // clientProcessRequestFinished method
-        }
+        if (!tunnelOnError(ERR_INVALID_URL))
+            setReplyError(context, ERR_INVALID_URL, Http::scBadRequest, requestErrorBytes);
         return false;
     }
 
@@ -160,10 +153,8 @@ Http::One::Server::buildHttpRequest(Http::StreamPointer &context)
         http->setLogUriToRawUri(http->uri, parser_->method());
 
         const char * requestErrorBytes = nullptr; //HttpParserHdrBuf(parser_);
-        if (!tunnelOnError(ERR_UNSUP_HTTPVERSION)) {
-            setReplyError(context, request, ERR_UNSUP_HTTPVERSION, Http::scHttpVersionNotSupported, requestErrorBytes);
-            clientProcessRequestFinished(this, request);
-        }
+        if (!tunnelOnError(ERR_UNSUP_HTTPVERSION))
+            setReplyError(context, ERR_UNSUP_HTTPVERSION, Http::scHttpVersionNotSupported, requestErrorBytes);
         return false;
     }
 
@@ -173,10 +164,8 @@ Http::One::Server::buildHttpRequest(Http::StreamPointer &context)
         // setReplyToError() requires log_uri
         http->setLogUriToRawUri(http->uri, parser_->method());
         const char * requestErrorBytes = nullptr; //HttpParserHdrBuf(parser_);
-        if (!tunnelOnError(ERR_INVALID_REQ)) {
-            setReplyError(context, request, ERR_INVALID_REQ, Http::scBadRequest, requestErrorBytes);
-            clientProcessRequestFinished(this, request);
-        }
+        if (!tunnelOnError(ERR_INVALID_REQ))
+            setReplyError(context, ERR_INVALID_REQ, Http::scBadRequest, requestErrorBytes);
         return false;
     }
 
@@ -187,35 +176,36 @@ Http::One::Server::buildHttpRequest(Http::StreamPointer &context)
     if (request->header.has(Http::HdrType::HOST))
         request->header.updateOrAddStr(Http::HdrType::HOST, request->url.authority());
 
-    // TODO: We fill request notes here until we find a way to verify whether
-    // no ACL checking is performed before ClientHttpRequest::doCallouts().
-    if (hasNotes()) {
-        assert(!request->hasNotes());
-        request->notes()->append(notes().getRaw());
-    }
-
     http->initRequest(request.getRaw());
+    request->manager(this, http->al);
 
     return true;
 }
 
 void
-Http::One::Server::setReplyError(Http::StreamPointer &context, HttpRequest::Pointer &request, err_type requestError, Http::StatusCode errStatusCode, const char *requestErrorBytes)
+Http::One::Server::setReplyError(Http::StreamPointer &context, err_type requestError, Http::StatusCode errStatusCode, const char *requestErrorBytes)
 {
-    quitAfterError(request.getRaw());
+    quitAfterError(nullptr);
     if (!context->connRegistered()) {
         debugs(33, 2, "Client stream deregister it self, nothing to do");
         clientConnection->close();
         return;
     }
+    auto http = context->http;
     clientStreamNode *node = context->getClientReplyContext();
     clientReplyContext *repContext = dynamic_cast<clientReplyContext *>(node->data.getRaw());
     assert (repContext);
 
-    repContext->setReplyToError(requestError, errStatusCode, context->http->uri, this, nullptr, requestErrorBytes, nullptr);
+    repContext->setReplyToError(requestError, errStatusCode, http->uri, this, nullptr, requestErrorBytes, nullptr);
 
-    assert(context->http->out.offset == 0);
+    assert(http->out.offset == 0);
     context->pullData();
+
+    if (auto request = http->request) {
+        // clientReplyContext::createStoreEntry() have created the 'catch-all' request
+        request->manager(this, http->al);
+        clientProcessRequestFinished(this, request);
+    }
 }
 
 void

src/servers/Http1Server.h
@@ -59,7 +59,7 @@ private:
     /// Return false if parsing is failed, true otherwise.
     bool buildHttpRequest(Http::StreamPointer &context);
 
-    void setReplyError(Http::StreamPointer &context, HttpRequest::Pointer &request, err_type requestError, Http::StatusCode errStatusCode, const char *requestErrorBytes);
+    void setReplyError(Http::StreamPointer &context, err_type requestError, Http::StatusCode errStatusCode, const char *requestErrorBytes);
 
     Http1::RequestParserPointer parser_;
     HttpRequestMethod method_; ///< parsed HTTP method