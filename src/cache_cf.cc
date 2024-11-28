5a24c6592362d9f3bd21e7d75617a19b2286b6ad
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2024-11-29 02:03:11 +0300

    Initial on_error implementation

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -254,6 +254,9 @@ static void free_configuration_includes_quoted_values(bool *recognizeQuotedValue
 static void parse_on_unsupported_protocol(acl_access **access);
 static void dump_on_unsupported_protocol(StoreEntry *entry, const char *name, acl_access *access);
 static void free_on_unsupported_protocol(acl_access **access);
+static void parse_on_error(acl_access **);
+static void dump_on_error(StoreEntry *, const char *directiveName, const acl_access *);
+static void free_on_error(acl_access **);
 static void ParseAclWithAction(acl_access **access, const Acl::Answer &action, const char *desc, Acl::Node *acl = nullptr);
 static void parse_http_upgrade_request_protocols(HttpUpgradeProtocolAccess **protoGuards);
 static void dump_http_upgrade_request_protocols(StoreEntry *entry, const char *name, HttpUpgradeProtocolAccess *protoGuards);
@@ -4907,6 +4910,60 @@ free_on_unsupported_protocol(acl_access **access)
 {
     free_acl_access(access);
 }
+
+// TODO: Reduce code duplication with parse_on_unsupported_protocol()
+static void
+parse_on_error(acl_access ** const access)
+{
+    const auto actionName = LegacyParser.token("action name");
+
+    // XXX: Add an actions enum (while moving to Configuration::?)
+    // XXX: Reduce code duplication with dump_on_error()
+    auto action = Acl::Answer(ACCESS_ALLOWED);
+    if (actionName.cmp("gracefully_close") == 0)
+        action.kind = 1;
+    else if (actionName.cmp("reset") == 0)
+        action.kind = 2;
+    else if (actionName.cmp("respond") == 0)
+        action.kind = 3;
+    else
+        throw TextException(ToSBuf("unknown action name: ", actionName), Here());
+
+    if (LegacyParser.skipOptional("if"))
+        return ParseAclWithAction(access, action, cfg_directive);
+
+    // OK: unconditional action; TODO: Warn if more actions follow.
+
+    // XXX: Reject empty rules: `on_error reset if`
+
+    // call to populate Config.accessList.onError even if there are no ACLs
+    ParseAclWithAction(access, action, cfg_directive);
+}
+
+static void
+dump_on_error(StoreEntry * const entry, const char * const directiveName, const acl_access * const access)
+{
+    static const std::vector<const char *> actionNames = {
+        "none",
+        "gracefully_close",
+        "reset",
+        "respond"
+    };
+    if (access) {
+        const auto actionsWithAclNames = ToTree(access).treeDump(directiveName, [](const Acl::Answer &action) {
+            Assure(action.kind > 0);
+            return actionNames.at(action.kind);
+        });
+        dump_SBufList(entry, actionsWithAclNames);
+    }
+}
+
+static void
+free_on_error(acl_access ** const access)
+{
+    free_acl_access(access);
+}
+
 
 static void
 parse_http_upgrade_request_protocols(HttpUpgradeProtocolAccess **protoGuardsPtr)
