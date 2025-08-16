81ff92e0f78e7a187ba00d09bbfa2cadb9681a22
Alex Rousskov <rousskov@measurement-factory.com>
2025-08-16 11:19:44 -0400

    fixup: Made "make check" happy

---
+++ src/tests/stub_StrandKid.cc
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 1996-2022 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#include "squid.h"
+
+#define STUB_API "StrandKid.cc"
+#include "tests/STUB.h"
+
+#include "StrandKid.h"
+void InitStrand() STUB
+void TagStrand(const SBuf &) STUB
+void StrandBarrierWait(const AsyncCallPointer &) STUB
+
