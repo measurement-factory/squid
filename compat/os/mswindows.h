c8e7ae320df167dee618b93d788ec9ab69e3d37d
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2025-09-27 01:40:30 +0300

    Created Windows substitution for setenv()

--- compat/os/mswindows.h
+++ compat/os/mswindows.h
@@ -107,6 +107,7 @@ SQUIDCEXTERN int WIN32_truncate(const char *pathname, off_t length);
 #define mkdir(p,F) mkdir((p))
 #define pclose _pclose
 #define popen _popen
+#define setenv(n,v,o) _putenv_s((n),(v))
 #define setmode _setmode
 #define sleep(t) Sleep((t)*1000)
 #define umask _umask
