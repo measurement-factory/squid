measurement-factory/squid

commit 6aa60f43322a48fd8aeea07da95a55ccffe73642

Merge Server class back into ConnStateData
Extracted from ConnStateData in 2015 commit fcc444e3, the Server class
was meant to be a "common base for all Server classes" that "manages
connections from clients". That proposed design has not been implemented
and is arguably problematic: Specific Server classes should not have
HTTP/1-specific Server::pipeline as a common denominator; to implement
HTTP/3, TLS-inside-TLS, and other complex/varying protocol combinations,
they are likely to treat low-level TCP connection I/O code as just one
of available protocol _layers_ rather than a hard-coded parent class.

Most implemented Server methods were HTTP/1 or TCP-specific, outsourcing
difficult connection management work to ConnStateData and its children
instead of implementing that logic inside the class meant to "manage
connections". Calling pure virtual methods aside, Server::doClientRead()
and clientWriteDone() methods did some basic TCP I/O layer operations.
Similar code should be eventually extracted into an optional/switchable
protocol layer class, without the heavy high-level pipeline baggage.

XXX: To reduce the number of merge conflicts in complicated code, this
change leaves non-inlined/non-overloaded methods in server/Server.cc and
keeps the old class name using a Server.cc-specific macro.

----

Merged (fast-forward eligible) SQUID-1089-merge-Server-class-back as of
commit c434a332.

src/servers/Server.cc
@@ -7,7 +7,6 @@
  */
 
 #include "squid.h"
-#include "anyp/PortCfg.h"
 #include "client_side.h"
 #include "comm.h"
 #include "comm/Read.h"
@@ -15,46 +14,12 @@
 #include "error/SysErrorDetail.h"
 #include "fd.h"
 #include "fde.h"
-#include "http/Stream.h"
-#include "LogTags.h"
-#include "MasterXaction.h"
-#include "servers/Server.h"
 #include "SquidConfig.h"
 #include "StatCounters.h"
 #include "tools.h"
 
-Server::Server(const MasterXaction::Pointer &xact) :
-    AsyncJob("::Server"), // kids overwrite
-    clientConnection(xact->tcpClient),
-    transferProtocol(xact->squidPort->transport),
-    port(xact->squidPort),
-    receivedFirstByte_(false)
-{
-    clientConnection->leaveOrphanage();
-}
-
-bool
-Server::doneAll() const
-{
-    // servers are not done while the connection is open
-    return !Comm::IsConnOpen(clientConnection) &&
-           BodyProducer::doneAll();
-}
-
-void
-Server::start()
-{
-    // TODO: shuffle activity from ConnStateData
-}
-
-void
-Server::swanSong()
-{
-    if (Comm::IsConnOpen(clientConnection))
-        clientConnection->close();
-
-    BodyProducer::swanSong();
-}
+// XXX: Diff reducer
+#define Server ConnStateData
 
 void
 Server::stopReading()
@@ -201,12 +166,7 @@ Server::doClientRead(const CommIoCbParams &io)
     afterClientRead();
 }
 
-/** callback handling the Comm::Write completion
- *
- * Will call afterClientWrite(size_t) to sync the I/O state.
- * Then writeSomeData() to initiate any followup writes that
- * could be immediately done.
- */
+/// callback handling the Comm::Write completion
 void
 Server::clientWriteDone(const CommIoCbParams &io)
 {
@@ -229,6 +189,5 @@ Server::clientWriteDone(const CommIoCbParams &io)
     }
 
     afterClientWrite(io.size); // update state
-    writeSomeData(); // maybe schedules another write
 }
 

src/client_side.cc
@@ -601,7 +601,10 @@ ConnStateData::swanSong()
     // XXX: Closing pinned conn is too harsh: The Client may want to continue!
     unpinConnection(true);
 
-    Server::swanSong();
+    if (Comm::IsConnOpen(clientConnection))
+        clientConnection->close();
+
+    BodyProducer::swanSong();
 
 #if USE_AUTH
     // NP: do this bit after closing the connections to avoid side effects from unwanted TCP RST
@@ -614,7 +617,7 @@ ConnStateData::swanSong()
 void
 ConnStateData::callException(const std::exception &ex)
 {
-    Server::callException(ex); // logs ex and stops the job
+    AsyncJob::callException(ex); // logs ex and stops the job
 
     ErrorDetail::Pointer errorDetail;
     if (const auto tex = dynamic_cast<const TextException*>(&ex))
@@ -979,6 +982,7 @@ ConnStateData::stopSending(const char *error)
     clientConnection->close();
 }
 
+/// processing to sync state after a Comm::Write()
 void
 ConnStateData::afterClientWrite(size_t size)
 {
@@ -1385,6 +1389,7 @@ ConnStateData::parseHttpRequest(const Http1::RequestParserPointer &hp)
     return result;
 }
 
+/// whether to stop serving our client after reading EOF on its connection
 bool
 ConnStateData::shouldCloseOnEof() const
 {
@@ -1820,6 +1825,7 @@ ConnStateData::parseProxyProtocolHeader()
     return true;
 }
 
+/// Update flags and timeout after the first byte received
 void
 ConnStateData::receivedFirstByte()
 {
@@ -1906,6 +1912,7 @@ ConnStateData::parseRequests()
     }
 }
 
+/// processing to be done after a Comm::Read()
 void
 ConnStateData::afterClientRead()
 {
@@ -2110,11 +2117,16 @@ ConnStateData::lifetimeTimeout(const CommTimeoutCbParams &io)
 
 ConnStateData::ConnStateData(const MasterXaction::Pointer &xact) :
     AsyncJob("ConnStateData"), // kids overwrite
-    Server(xact)
 #if USE_OPENSSL
-    , tlsParser(Security::HandshakeParser::fromClient)
+    tlsParser(Security::HandshakeParser::fromClient),
 #endif
+    clientConnection(xact->tcpClient),
+    transferProtocol(xact->squidPort->transport),
+    port(xact->squidPort),
+    receivedFirstByte_(false)
 {
+    clientConnection->leaveOrphanage();
+
     // store the details required for creating more MasterXaction objects as new requests come in
     log_addr = xact->tcpClient->remote;
     log_addr.applyClientMask(Config.Addrs.client_netmask);
@@ -3512,6 +3524,22 @@ ConnStateData::fillConnectionLevelDetails(ACLFilledChecklist &checklist) const
 #endif
 }
 
+void
+ConnStateData::write(MemBuf *mb)
+{
+    typedef CommCbMemFunT<ConnStateData, CommIoCbParams> Dialer;
+    writer = JobCallback(33, 5, Dialer, this, ConnStateData::clientWriteDone);
+    Comm::Write(clientConnection, mb, writer);
+}
+
+void
+ConnStateData::write(char *buf, int len)
+{
+    typedef CommCbMemFunT<ConnStateData, CommIoCbParams> Dialer;
+    writer = JobCallback(33, 5, Dialer, this, ConnStateData::clientWriteDone);
+    Comm::Write(clientConnection, buf, len, writer, nullptr);
+}
+
 bool
 ConnStateData::transparent() const
 {
@@ -3903,6 +3931,7 @@ ConnStateData::unpinConnection(const bool andClose)
      * connection has gone away */
 }
 
+/// abort any pending transactions and prevent new ones (by closing)
 void
 ConnStateData::terminateAll(const Error &rawError, const LogTagsErrors &lte)
 {

src/client_side.h
@@ -12,18 +12,27 @@
 #define SQUID_SRC_CLIENT_SIDE_H
 
 #include "acl/ChecklistFiller.h"
+#include "anyp/forward.h"
+#include "anyp/ProtocolVersion.h"
+#include "base/AsyncJob.h"
 #include "base/RunnersRegistry.h"
+#include "BodyPipe.h"
 #include "clientStreamForward.h"
 #include "comm.h"
+#include "comm/Write.h"
+#include "CommCalls.h"
 #include "error/Error.h"
+#include "error/forward.h"
 #include "helper/forward.h"
 #include "http/forward.h"
+#include "http/Stream.h"
 #include "HttpControlMsg.h"
 #include "ipc/FdNotes.h"
 #include "log/forward.h"
+#include "Pipeline.h"
 #include "proxyp/forward.h"
 #include "sbuf/SBuf.h"
-#include "servers/Server.h"
+#include "servers/forward.h"
 #if USE_AUTH
 #include "auth/UserRequest.h"
 #endif
@@ -77,7 +86,8 @@ class ServerBump;
  * stopSending() methods.
  */
 class ConnStateData:
-    public Server,
+    virtual public AsyncJob,
+    public BodyProducer,
     public HttpControlMsgSink,
     public Acl::ChecklistFiller,
     private IndependentRunner
@@ -87,12 +97,19 @@ public:
     explicit ConnStateData(const MasterXactionPointer &xact);
     ~ConnStateData() override;
 
-    /* ::Server API */
-    void receivedFirstByte() override;
-    bool handleReadData() override;
-    void afterClientRead() override;
-    void afterClientWrite(size_t) override;
+    /// maybe grow the inBuf and schedule Comm::Read()
+    void readSomeData();
 
+    /// cancels Comm::Read() if it is scheduled
+    void stopReading();
+
+    /// schedule some data for a Comm::Write()
+    void write(MemBuf *mb);
+
+    /// schedule some data for a Comm::Write()
+    void write(char *buf, int len);
+
+public:
     /* HttpControlMsgSink API */
     void sendControlMsg(HttpControlMsg) override;
     void doneWithControlMsg() override;
@@ -383,7 +400,33 @@ public:
     /// managers logging of the being-accepted TLS connection secrets
     Security::KeyLogger keyLogger;
 
+// XXX: should be 'protected:' for child access only,
+//      but all sorts of code likes to play directly
+//      with the I/O buffers and socket.
+public:
+    // Client TCP connection details from comm layer.
+    Comm::ConnectionPointer clientConnection;
+
+    /**
+     * The transfer protocol currently being spoken on this connection.
+     * HTTP/1.x CONNECT, HTTP/1.1 Upgrade and HTTP/2 SETTINGS offer the
+     * ability to change protocols on the fly.
+     */
+    AnyP::ProtocolVersion transferProtocol;
+
+    /// Squid listening port details where this connection arrived.
+    AnyP::PortCfgPointer port;
+
+    /// read I/O buffer for the client connection
+    SBuf inBuf;
+
+    /// set of requests waiting to be serviced
+    Pipeline pipeline;
+
 protected:
+    /// whether client_request_buffer_max_size allows inBuf.length() increase
+    bool mayBufferMoreRequestBytes() const;
+
     void startDechunkingRequest();
     void finishDechunkingRequest(bool withSuccess);
     void abortChunkedRequestBody(const err_type error);
@@ -435,10 +478,21 @@ protected:
 
     bool tunnelOnError(const err_type);
 
+    AsyncCall::Pointer reader; ///< set when we are reading
+    AsyncCall::Pointer writer; ///< set when we are writing
+
 private:
-    /* ::Server API */
-    void terminateAll(const Error &, const LogTagsErrors &) override;
-    bool shouldCloseOnEof() const override;
+    void terminateAll(const Error &, const LogTagsErrors &);
+    bool shouldCloseOnEof() const;
+    void doClientRead(const CommIoCbParams &);
+    void clientWriteDone(const CommIoCbParams &);
+    void maybeMakeSpaceAvailable();
+
+    /// whether Comm::Read() is scheduled
+    bool reading() const { return reader != nullptr; }
+
+    /// whether Comm::Write() is scheduled
+    bool writing() const { return writer != nullptr; }
 
     void checkLogging();
 
@@ -447,6 +501,11 @@ private:
     bool concurrentRequestQueueFilled() const;
 
     void pinConnection(const Comm::ConnectionPointer &pinServerConn, const HttpRequest &request);
+
+    void receivedFirstByte();
+    bool handleReadData();
+    void afterClientRead();
+    void afterClientWrite(size_t);
 
     /* PROXY protocol functionality */
     bool proxyProtocolValidateClient();
@@ -494,6 +553,8 @@ private:
     Ssl::ServerBump *sslServerBump = nullptr;
     Ssl::CertSignAlgorithm signAlgorithm = Ssl::algSignTrusted; ///< The signing algorithm to use
 #endif
+
+    bool receivedFirstByte_; ///< true if at least one byte received on this connection
 
     /// the reason why we no longer write the response or nil
     const char *stoppedSending_ = nullptr;

src/servers/Makefile.am
@@ -15,5 +15,4 @@ libservers_la_SOURCES = \
 	Http1Server.cc \
 	Http1Server.h \
 	Server.cc \
-	Server.h \
 	forward.h

src/servers/Server.h
@@ -1,135 +1,0 @@
-/*
- * Copyright (C) 1996-2025 The Squid Software Foundation and contributors
- *
- * Squid software is distributed under GPLv2+ license and includes
- * contributions from numerous individuals and organizations.
- * Please see the COPYING and CONTRIBUTORS files for details.
- */
-
-/* DEBUG: section 33    Client-side Routines */
-
-#ifndef SQUID_SRC_SERVERS_SERVER_H
-#define SQUID_SRC_SERVERS_SERVER_H
-
-#include "anyp/forward.h"
-#include "anyp/ProtocolVersion.h"
-#include "base/AsyncJob.h"
-#include "BodyPipe.h"
-#include "comm/Write.h"
-#include "CommCalls.h"
-#include "error/forward.h"
-#include "http/Stream.h"
-#include "log/forward.h"
-#include "Pipeline.h"
-#include "sbuf/SBuf.h"
-#include "servers/forward.h"
-
-/**
- * Common base for all Server classes used
- * to manage connections from clients.
- */
-class Server : virtual public AsyncJob, public BodyProducer
-{
-public:
-    Server(const MasterXactionPointer &xact);
-    ~Server() override {}
-
-    /* AsyncJob API */
-    void start() override;
-    bool doneAll() const override;
-    void swanSong() override;
-
-    /// whether to stop serving our client after reading EOF on its connection
-    virtual bool shouldCloseOnEof() const = 0;
-
-    /// maybe grow the inBuf and schedule Comm::Read()
-    void readSomeData();
-
-    /**
-     * called when new request data has been read from the socket
-     *
-     * \retval false called comm_close or setReplyToError (the caller should bail)
-     * \retval true  we did not call comm_close or setReplyToError
-     */
-    virtual bool handleReadData() = 0;
-
-    /// processing to be done after a Comm::Read()
-    virtual void afterClientRead() = 0;
-
-    /// whether Comm::Read() is scheduled
-    bool reading() const {return reader != nullptr;}
-
-    /// cancels Comm::Read() if it is scheduled
-    void stopReading();
-
-    /// Update flags and timeout after the first byte received
-    virtual void receivedFirstByte() = 0;
-
-    /// maybe find some data to send and schedule a Comm::Write()
-    virtual void writeSomeData() {}
-
-    /// schedule some data for a Comm::Write()
-    void write(MemBuf *mb) {
-        typedef CommCbMemFunT<Server, CommIoCbParams> Dialer;
-        writer = JobCallback(33, 5, Dialer, this, Server::clientWriteDone);
-        Comm::Write(clientConnection, mb, writer);
-    }
-
-    /// schedule some data for a Comm::Write()
-    void write(char *buf, int len) {
-        typedef CommCbMemFunT<Server, CommIoCbParams> Dialer;
-        writer = JobCallback(33, 5, Dialer, this, Server::clientWriteDone);
-        Comm::Write(clientConnection, buf, len, writer, nullptr);
-    }
-
-    /// processing to sync state after a Comm::Write()
-    virtual void afterClientWrite(size_t) {}
-
-    /// whether Comm::Write() is scheduled
-    bool writing() const {return writer != nullptr;}
-
-// XXX: should be 'protected:' for child access only,
-//      but all sorts of code likes to play directly
-//      with the I/O buffers and socket.
-public:
-
-    /// grows the available read buffer space (if possible)
-    void maybeMakeSpaceAvailable();
-
-    // Client TCP connection details from comm layer.
-    Comm::ConnectionPointer clientConnection;
-
-    /**
-     * The transfer protocol currently being spoken on this connection.
-     * HTTP/1.x CONNECT, HTTP/1.1 Upgrade and HTTP/2 SETTINGS offer the
-     * ability to change protocols on the fly.
-     */
-    AnyP::ProtocolVersion transferProtocol;
-
-    /// Squid listening port details where this connection arrived.
-    AnyP::PortCfgPointer port;
-
-    /// read I/O buffer for the client connection
-    SBuf inBuf;
-
-    bool receivedFirstByte_; ///< true if at least one byte received on this connection
-
-    /// set of requests waiting to be serviced
-    Pipeline pipeline;
-
-protected:
-    /// abort any pending transactions and prevent new ones (by closing)
-    virtual void terminateAll(const Error &, const LogTagsErrors &) = 0;
-
-    /// whether client_request_buffer_max_size allows inBuf.length() increase
-    bool mayBufferMoreRequestBytes() const;
-
-    void doClientRead(const CommIoCbParams &io);
-    void clientWriteDone(const CommIoCbParams &io);
-
-    AsyncCall::Pointer reader; ///< set when we are reading
-    AsyncCall::Pointer writer; ///< set when we are writing
-};
-
-#endif /* SQUID_SRC_SERVERS_SERVER_H */
-

commit 6a2d9f7dc3fb629c7557a3df56aee37c5d279efb

fixup: Fix layer-01-minimal build by restoring a required include
    Server.cc:162:82: error: incomplete type LogTagsErrors

Probably broken in recent commit 6aa60f43.

src/servers/Server.cc
@@ -14,6 +14,7 @@
 #include "error/SysErrorDetail.h"
 #include "fd.h"
 #include "fde.h"
+#include "LogTags.h"
 #include "SquidConfig.h"
 #include "StatCounters.h"
 #include "tools.h"