f1029525a3cfefd829d60aef975fe8e6e172d03f
Alex Rousskov <rousskov@measurement-factory.com>
2026-02-06 16:42:06 -0500

    fixup: Reduce duplication of YAML indenting code
    compared to
    
        yaml << spaces<3*4>() << "- {...} ";
    
    I like how
    
        yaml << yamlIndent() << yamlIndent() << yamlIndent() << "- {...} ";
    
    visualizes/emphasizes the number of indentation levels, but I do not
    like `yaml << yaml... << yaml...` repetition. With a bit more code, we
    could arrive at something even more readable and succinct like
    
        yaml << yamlIndent*3 << "- {...} ";
    
    but, hopefully, we will eventually get rid of the need to manually
    indent (several levels deep) instead. Until then, we need to find the
    best manual option we can agree on. More on that at
    https://github.com/squid-cache/squid/pull/1735#discussion_r1557659176
    
    Sandbox: https://godbolt.org/z/ernjd6956

--- src/ssl/MemStats.cc
+++ src/ssl/MemStats.cc
@@ -18,6 +18,23 @@ Ssl::MemAllocStats::MemAllocStats(const char * const aDescription):
     allocations.logInit(20, 0, 1024*1024);
 }
 
+/// a [compile-time] string containing exactly N spaces
+template <size_t N>
+constexpr auto
+spaces()
+{
+    constexpr auto maximumSpace = "                                          ";
+
+    // we use a constexpr variant of strlen() to check whether N is small enough
+    constexpr auto maximumLength = std::char_traits<char>::length(maximumSpace);
+    static_assert(N <= maximumLength);
+
+    return maximumSpace + maximumLength - N;
+}
+
+/// Supplies one level of YAML indentation. Keep in sync with AsyncJob::ReportAllJobs().
+template <size_t L = 1> constexpr auto yamlIndent() { return spaces<4*L>(); }
+
 /// YAML-compliant StatHistBinDumper for memory allocation stats
 static void
 BinToYamlListItem(StoreEntry * const e, int, const double minValue, const double bucketSize, const int valueCount)
@@ -26,10 +43,9 @@ BinToYamlListItem(StoreEntry * const e, int, const double minValue, const double
         return;
 
     PackableStream yaml(*e);
-    const auto indent = "      ";
     // our allocations.logInit() limits sizes to 7 digits; valueCount may have 10
     const auto setw = [](std::ostream &os) -> auto& { return os << std::setw(10); };
-    yaml << indent << indent << indent << "- { " <<
+    yaml << yamlIndent() << yamlIndent() << yamlIndent() << "- { " <<
          "min: " << setw << static_cast<uint64_t>(minValue) << ", " <<
          "max: " << setw << (static_cast<uint64_t>(minValue+bucketSize)-1) << ", " <<
          "count: " << setw << valueCount << " }\n";
@@ -39,11 +55,10 @@ void
 Ssl::MemAllocStats::dump(StoreEntry &e)
 {
     PackableStream yaml(e);
-    const auto indent = "      ";
-    yaml << indent << "stats for " << description << ":\n";
-    yaml << indent << indent << "calls: " << allocations.valuesCounted() << "\n";
+    yaml << yamlIndent() << "stats for " << description << ":\n";
+    yaml << yamlIndent() << yamlIndent() << "calls: " << allocations.valuesCounted() << "\n";
     if (allocations.valuesCounted())
-        yaml << indent << indent << "allocation size histogram (bytes):" << "\n";
+        yaml << yamlIndent() << yamlIndent() << "allocation size histogram (bytes):" << "\n";
     yaml.flush();
     allocations.dump(&e, &BinToYamlListItem);
 }
@@ -80,7 +95,6 @@ void
 Ssl::ReportMemoryStats(StoreEntry &e)
 {
     PackableStream yaml(e);
-    const auto indent = "      ";
 
     yaml << "OpenSSL memory usage:\n";
 
@@ -88,11 +102,11 @@ Ssl::ReportMemoryStats(StoreEntry &e)
     const auto allocated = MallocStats().allocationsCounted();
     const auto freed = FreeStats();
     if (allocated >= freed)
-        yaml << indent << "in-use allocations: " << (allocated - freed) << "\n";
+        yaml << yamlIndent() << "in-use allocations: " << (allocated - freed) << "\n";
 
     // match MallocStats() reporting style even though we cannot report a histogram
-    yaml << indent << "stats for free():" << "\n";
-    yaml << indent << indent << "calls: " << freed << "\n";
+    yaml << yamlIndent() << "stats for free():" << "\n";
+    yaml << yamlIndent() << yamlIndent() << "calls: " << freed << "\n";
 
     yaml.flush();
 
