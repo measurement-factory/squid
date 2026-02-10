3079af574fd0a9165678d38aa8959a4873f611f8
Ricardo Ferreira Ribeiro <garb12@pm.me>
2026-02-10 08:23:37 +0000

    Revert "Tolerate mismatching CONNECT Host for intercepted connections"
    This reverts commit b599b05fd917b3aec62f6de1242439379aa30d6b.

--- src/client_side_request.cc
+++ src/client_side_request.cc
@@ -390,8 +390,7 @@ ClientRequestContext::hostHeaderVerifyFailed(const char *A, const char *B)
 {
     // IP address validation for Host: failed. Admin wants to ignore them.
     // NP: we do not yet handle CONNECT tunnels well, so ignore for them
-    if ((!Config.onoff.hostStrictVerify && http->request->method != Http::METHOD_CONNECT) ||
-	(http->request->method == Http::METHOD_CONNECT && (http->request->flags.intercepted || http->request->flags.interceptTproxy))) {
+    if (!Config.onoff.hostStrictVerify && http->request->method != Http::METHOD_CONNECT) {
         debugs(85, 3, "SECURITY ALERT: Host header forgery detected on " << http->getConn()->clientConnection <<
                " (" << A << " does not match " << B << ") on URL: " << http->request->effectiveRequestUri());
 
