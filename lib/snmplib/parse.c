83cfbce3f6d3820b1ac300c88cb8a68a965297e7
Eduard Bagdasaryan <dictiano@gmail.com>
2026-02-12 14:43:08 +0300

    Removed unused files, try 4

--- lib/snmplib/parse.c
+++ lib/snmplib/parse.c
@@ -401,67 +401,6 @@ get_token(register FILE *fp, register char *token)
     return ENDOFFILE;
 }
 
-/*
- * Takes a list of the form:
- * { iso org(3) dod(6) 1 }
- * and creates several nodes, one for each parent-child pair.
- * Returns NULL on error.
- *   register struct subid *SubOid; an array of subids
- *   int length;            the length of the array
- */
-static int
-getoid(register FILE *fp, register struct subid *SubOid, int length)
-{
-    register int count;
-    int type;
-    char token[128];
-    register char *cp;
-
-    if ((type = get_token(fp, token)) != LEFTBRACKET) {
-        print_error("Expected \"{\"", token, type);
-        return 0;
-    }
-    type = get_token(fp, token);
-    for (count = 0; count < length; count++, SubOid++) {
-        SubOid->label = 0;
-        SubOid->subid = -1;
-        if (type == RIGHTBRACKET) {
-            return count;
-        } else if (type != LABEL && type != NUMBER) {
-            print_error("Not valid for object identifier", token, type);
-            return 0;
-        }
-        if (type == LABEL) {
-            /* this entry has a label */
-            cp = (char *) xmalloc((unsigned) strlen(token) + 1);
-            strcpy(cp, token);
-            SubOid->label = cp;
-            type = get_token(fp, token);
-            if (type == LEFTPAREN) {
-                type = get_token(fp, token);
-                if (type == NUMBER) {
-                    SubOid->subid = atoi(token);
-                    if ((type = get_token(fp, token)) != RIGHTPAREN) {
-                        print_error("Unexpected a closing parenthesis", token, type);
-                        return 0;
-                    }
-                } else {
-                    print_error("Expected a number", token, type);
-                    return 0;
-                }
-            } else {
-                continue;
-            }
-        } else {
-            /* this entry  has just an integer sub-identifier */
-            SubOid->subid = atoi(token);
-        }
-        type = get_token(fp, token);
-    }
-    return count;
-
-}
-
 static void
 free_node(struct node *np)
 {
@@ -474,15 +413,5 @@ free_node(struct node *np)
         xfree((char *) tep);
     }
     xfree((char *) np);
-}
-
-static void
-free_node_list(struct node *nl)
-{
-    while (nl) {
-        struct node *t = nl->next;
-        free_node(nl);
-        nl = t;
-    }
 }
 
