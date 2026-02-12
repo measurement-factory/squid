0cadedf5fbf4f4ffcb435c34e47024452264da27
Eduard Bagdasaryan <dictiano@gmail.com>
2026-02-12 13:08:19 +0300

    Many fixes and adjustments
    * analyze 'used' lines as well and report inconsistencies
    * fixed function name regex
    * skip leading empty lines
    * handle 'resolvable' conflicts, like MACROs defining both
      used and unused functions

--- test-suite/remove-unused.pl
+++ test-suite/remove-unused.pl
@@ -2,38 +2,57 @@
 use strict;
 use warnings;
 
-my $currentUnusedFunction = undef;
-my %fileRanges;
+my $currentFunction = undef;
+my %fileRangesUsed;
+my %fileRangesUnused;
 
-sub addRangeToFile {
-    my ($fileName, $newStart, $newEnd) = @_;
+sub addRangeToFileUnused {
+    my ($fileName, $newStart, $newEnd, $fileRanges) = @_;
+    my $isUnused = ($fileRanges == \%fileRangesUnused);
+    my $str = $isUnused ? "unused" : "used";
+    print "'$fileName': adding ranges $newStart,$newEnd to $str\n";
     if ($newStart < 1 && $newStart > $newEnd) {
         die "FATAL: in $fileName: wrong range: $newStart > $newEnd\n";
     }
-    if (exists $fileRanges{$fileName}) {
-        foreach my $range (@{$fileRanges{$fileName}}) {
+    if (exists $fileRanges->{$fileName}) {
+        my $pairsRef = $fileRanges->{$fileName};
+        foreach my $range (@$pairsRef) {
             my ($existStart, $existEnd) = @$range;
-            
-            if ($newStart <= $existEnd && $newEnd >= $existStart) {
-                die "FATAL: in $fileName: Range [$newStart, $newEnd] overlaps with [$existStart, $existEnd]\n";
+
+            if ($newStart == $existEnd && $newEnd == $existStart) {
+                print "'$fileName': skipping duplicate $existStart,$existEnd range \n";
+                return;
+            }
+
+            if ($isUnused) {
+                if ($newStart <= $existEnd && $newEnd >= $existStart) {
+                    die "FATAL: in $fileName: Range [$newStart, $newEnd] overlaps with [$existStart, $existEnd]\n";
+                }
             }
         }
     }
-    push @{$fileRanges{$fileName}}, [$newStart, $newEnd];
+    push @{$fileRanges->{$fileName}}, [$newStart, $newEnd];
 }
 
 my $inputFile = shift or die "Usage: $0 <input_file>\n";
 open(my $ih, '<', $inputFile) or die "Could not open $inputFile: $!";
 
+my $fileRanges = undef;
 while (my $line = <$ih>) {
     print "Processing '$line'\n";
-    if ($line =~ /^.+:\d+: warning: Function '(\w+)' is unused$/) {
-        $currentUnusedFunction = $1;
+    if ($line =~ /^.+:\d+: warning: Function '(.+)' is unused$/) {
+        $currentFunction = $1;
+        $fileRanges = \%fileRangesUnused;
+        next;
+    }
+    if ($line =~ /^.+:\d+: note: Function '(.+)' uses=\d+$/) {
+        $currentFunction = $1;
+        $fileRanges = \%fileRangesUsed;
         next;
     }
 
-    if (defined $currentUnusedFunction && $line =~ /^([^:\s]+):(\d+): note: (defined|declared|comment starts) here$/) {
-        
+    if (defined $currentFunction && $line =~ /^([^:\s]+):(\d+): note: (defined|declared|comment starts) here$/) {
+
         my $file = $1;
         my $start = $2;
         my $type = $3;
@@ -59,19 +78,21 @@ while (my $line = <$ih>) {
                die "FATAL: file name mismatch in $line and $nextLine";
             }
             my $end = $2;
-            
-            addRangeToFile($file, $start, $end);
+
+            defined($fileRanges) or die "Parse error in $start";
+            addRangeToFileUnused($file, $start, $end, $fileRanges);
         } else {
             die "FATAL: Expected 'note: $expectedSuffix' after line $start,\n" .
                 "but found: $nextLine";
         }
     } else {
-        $currentUnusedFunction = undef;
+        $currentFunction = undef;
+        $fileRanges = undef;
     }
 }
 
 # apply removals
-foreach my $file (sort keys %fileRanges) {
+foreach my $file (sort keys %fileRangesUnused) {
     if (!-e $file) {
         die "FATAL: '$file' not found.\n";
     }
@@ -79,15 +100,34 @@ foreach my $file (sort keys %fileRanges) {
     open(my $fh, '<', $file) or die "ERROR: Could not open '$file': $!\n";
     my @content = <$fh>;
     close($fh);
-    
+
     my $totalLines = scalar @content;
     my %linesToDelete;
 
-    foreach my $rangePair (@{$fileRanges{$file}}) {
+    my $usedRanges = $fileRangesUsed{$file};
+    my $overlapped = 0;
+
+    foreach my $rangePair (@{$fileRangesUnused{$file}}) {
         my ($s, $e) = @$rangePair;
 
         if ($e > $totalLines) {
             die "FATAL: Range $s-$e is out of bounds for '$file'.\n";
+        }
+
+        if (defined $usedRanges) {
+            foreach my $usedRange(@$usedRanges) {
+                my ($us, $ue) = @$usedRange;
+                if ($us == $s && $ue == $e) {
+                    print "'$file': overriding same-line unused range with used range (functions defined by the same macro?) at $s,$e \n";
+                    $s = 0;
+                    $e = 0;
+                    last;
+                }
+                if ($us <= $e && $ue >= $s) {
+                    print "WARN: in $file: Unused range [$s, $e] overlaps with used range [$us, $ue]\n";
+                    $overlapped = 1;
+                }
+            }
         }
 
         for (my $i = $s; $i <= $e; $i++) {
@@ -95,13 +135,41 @@ foreach my $file (sort keys %fileRanges) {
         }
     }
 
+    if ($overlapped) {
+        print "Skipping $file due to operlapping errors\n";
+        delete $fileRangesUnused{$file};
+        next;
+    }
+
     open(my $out, '>', $file) or die "ERROR: Cannot write to '$file': $!\n";
 
+    my $emptyLines = undef;
     for (my $i = 0; $i < $totalLines; $i++) {
+        chomp(my $line = $content[$i]);
+        if ($line eq "") {
+            if (defined $emptyLines) {
+                $emptyLines .= $content[$i];
+            } else {
+                $emptyLines = $content[$i];
+            }
+            next;
+        }
+
+        if (defined $emptyLines) {
+            if (!exists $linesToDelete{$i + 1}) {
+                print $out $emptyLines;
+            }
+            $emptyLines = undef;
+        }
+
         print $out $content[$i] unless exists $linesToDelete{$i + 1};
     }
+    # write empty line leftovers
+    if (defined $emptyLines) {
+        print $out $emptyLines;
+    }
     close($out);
-    
+
     print "Processed '$file': " . (scalar keys %linesToDelete) . " lines removed.\n";
 }
 
