31d628d0d524815b581a9e63497c09dccbf177c0
Eduard Bagdasaryan <dictiano@gmail.com>
2026-03-11 15:34:44 +0300

    Removed unused functions, 3

--- lib/snmplib/mib.c
+++ lib/snmplib/mib.c
@@ -91,30 +91,3 @@ oid RFC1066_MIB[] = {1, 3, 6, 1, 2, 1};
 unsigned char RFC1066_MIB_text[] = ".iso.org.dod.internet.mgmt.mib";
 struct snmp_mib_tree *Mib;
 
-static int
-lc_cmp(const char *s1, const char *s2)
-{
-    char c1, c2;
-
-    while (*s1 && *s2) {
-        if (xisupper(*s1))
-            c1 = xtolower(*s1);
-        else
-            c1 = *s1;
-        if (xisupper(*s2))
-            c2 = xtolower(*s2);
-        else
-            c2 = *s2;
-        if (c1 != c2)
-            return ((c1 - c2) > 0 ? 1 : -1);
-        s1++;
-        s2++;
-    }
-
-    if (*s1)
-        return -1;
-    if (*s2)
-        return 1;
-    return 0;
-}
-
