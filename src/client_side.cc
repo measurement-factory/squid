73c9f470257957a5df42dbf6979defcb8ac79694
Ricardo Ferreira Ribeiro <garb12@pm.me>
2026-03-17 16:21:22 +0100

    Revert "revert change"
    This reverts commit 79b002762a9a73eb339ce5ce43bdda398fa03830.

--- src/client_side.cc
+++ src/client_side.cc
@@ -974,8 +974,7 @@ ConnStateData::endingShutdown()
 
     // force the client connection to close immediately
     // swanSong() in the close handler will cleanup.
-    if (Comm::IsConnOpen(clientConnection))
-        clientConnection->close();
+    clientConnection->close();
 }
 
 char *
