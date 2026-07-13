0f6d1620c38337d865313886c3a8f037df052eb0
Ricardo Ferreira Ribeiro <garb12@pm.me>
2026-07-13 09:51:53 +0200

    more aligning

--- src/FwdState.cc
+++ src/FwdState.cc
@@ -1492,9 +1492,7 @@ getOutgoingAddress(HttpRequest * request, const Comm::ConnectionPointer &conn)
         conn->local.setIPv4();
 
     // Do not spoof the client's source address for a real client CONNECT;
-    const bool realClientConnect = request &&
-                                         request->method == Http::METHOD_CONNECT &&
-                                         !request->flags.fakeRequest;
+    const bool realClientConnect = request && request->method == Http::METHOD_CONNECT && !request->flags.fakeRequest;
 
     // maybe use TPROXY client address
     if (request && request->flags.spoofClientIp && !realClientConnect) {
