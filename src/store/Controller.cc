c94de112f9a8b757d6c9dfa58a5b7789ee1a2994
Alex Rousskov <rousskov@measurement-factory.com>
2026-09-16 11:08:24 -0400

    Revert "Avoid crashes when Transients refresh results in "writer collision""
    This reverts previous branch commit 41b93f92. The fix will be improved
    using a dedicated SQUID-1176-SMP-cr-writer-collision-fixup branch and
    merged back here.

--- src/store/Controller.cc
+++ src/store/Controller.cc
@@ -928,16 +928,9 @@ void
 Store::Controller::updateFinished(StoreEntry &e, const StoreEntry &e304, const Ipc::StoreMapAnchor::UpdateStatus updateStatus)
 {
     if (e.hasTransients()) {
-        auto finalStatus = updateStatus;
-        try {
-            if (updateStatus == Ipc::StoreMapAnchor::uApplied)
-                transients->refreshEntry(e);
-        } catch (...) {
-            debugs(20, 2, "Failed to refresh transients entry " << CurrentException);
-            finalStatus = Ipc::StoreMapAnchor::uFailed;
-        }
+        transients->refreshEntry(e);
         if (e304.isSmpCollapsedRevalidationInitiator())
-            transients->setUpdateStatus(e304.mem_obj->xitTable, finalStatus);
+            transients->setUpdateStatus(e304.mem_obj->xitTable, updateStatus);
     }
 }
 
